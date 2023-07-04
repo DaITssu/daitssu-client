ea0c18baa95f0d09c168c5065a1649d0508ed79b
ChoiSangwon <fdsa200@naver.com>
2023-07-04 16:05:21 +0900

    feat : add AriclePreview component

---
+++ src/components/Main/ArticlePreview.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import { Article } from '@/types/articleDTO';
+
+interface ArticlePreviewProps {
+  article: Article;
+}
+
+const ArticlePreview: React.FC<ArticlePreviewProps> = ({ article }) => {
+  return (
+    <div className="article-preview" key={article.slug}>
+      <div className="article-meta">
+        <Link href="profile.html">
+          <img src={article.author.image} alt="Author Image" />
+        </Link>
+        <div className="info">
+          <Link href="">{article.author.username}</Link>
+          <span className="date">{article.createdAt}</span>
+        </div>
+        <button className="btn btn-outline-primary btn-sm pull-xs-right">
+          <i className="ion-heart"></i> {article.favoritesCount}
+        </button>
+      </div>
+      <Link href="" className="preview-link">
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
+        <span>Read more...</span>
+        <ul className="tag-list">
+          {article.tagList.map((res, index) => (
+            <li
+              key={index}
+              className="tag-default tag-pill tag-outline ng-binding ng-scope"
+            >
+              {res}
+            </li>
+          ))}
+        </ul>
+      </Link>
+    </div>
+  );
+};
+
+export default ArticlePreview;
