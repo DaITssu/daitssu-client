b4f2ca1e7330340820949636794512a8b13dea70
ChoiSangwon <fdsa200@naver.com>
2023-07-05 11:28:28 +0900

    fix : 메인화면 함수이름 변경

---
+++ src/pages/api/getArticles.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const getArticles = async (page: number) => {
+  try {
+    const response = await axios.get('https://api.realworld.io/api/articles', {
+      params: {
+        limit: 10,
+        offset: page * 10,
+      },
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+
+    throw error;
+  }
+};
