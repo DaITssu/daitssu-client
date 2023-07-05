0f5b95e9969548b2cd32b39ad4c4dd800fb317d2
White-Patrasche <5338095@gmail.com>
2023-07-06 02:56:44 +0900

    feat: login api

---
+++ src/pages/api/login.ts
@@ -0,0 +1,23 @@
+import { User, UserResponse } from '../../models/UserDTO';
+
+export const loginUser = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}): Promise<UserResponse> => {
+  const response = await fetch('https://api.realworld.io/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (response.status !== 200) {
+    throw new Error('Login failed');
+  }
+
+  return response.json();
+};
