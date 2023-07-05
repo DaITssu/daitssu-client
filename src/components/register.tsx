4e3cd1a07c96b6714f58cbedf46b0c54c435ae83
ChoiSangwon <fdsa200@naver.com>
2023-07-05 15:10:16 +0900

    feat : 회원가입 UI 추가

---
+++ src/components/register.tsx
@@ -0,0 +1,102 @@
+import axios from 'axios';
+import Link from 'next/link';
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { useRouter } from 'next/router';
+
+const Register: React.FC = () => {
+  const router = useRouter();
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('https://api.realworld.io/api/users', {
+        user: {
+          username,
+          email,
+          password,
+        },
+      });
+
+      console.log('Registration successful:', response.data);
+      router.push('/');
+      // Perform success actions (e.g., redirect, show success message)
+    } catch (error) {
+      console.error('Registration failed:', error);
+      // Perform error actions (e.g., show error message)
+    }
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign up</h1>
+            <p className="text-xs-center">
+              <Link href="">Have an account?</Link>
+            </p>
+
+            <ul className="error-messages">
+              <li>That email is already taken</li>
+            </ul>
+
+            <form onSubmit={handleSubmit}>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Your Name"
+                  value={username}
+                  onChange={handleNameChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </fieldset>
+              <fieldset className="form-group">
+                <input
+                  className="form-control form-control-lg"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </fieldset>
+              <button
+                className="btn btn-lg btn-primary pull-xs-right"
+                type="submit"
+              >
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
