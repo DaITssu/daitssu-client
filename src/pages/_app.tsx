14079a14924fd24a8ccf5456546f31db10e240ab
White-Patrasche <5338095@gmail.com>
2023-07-06 10:58:25 +0900

    fix: use api call without react query temporarily

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,6 @@
 import '@/styles/globals.css';
-import { QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
-import { QueryClient } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {' '}
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
