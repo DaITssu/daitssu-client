33e5727e7563cd602ffdab49bd3c8da0baacb4a3
kweonsikyung <ekrk6632@naver.com>
2023-07-09 02:51:13 +0900

    add: design tokens

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+  SSU: {
+    blue: '#5ebeeb',
+    skyblue: '#e7f5fc',
+    red: '#f36262',
+  },
+  grayscale: {
+    white: '#FFFFFF',
+    Gray1: '#6E6E6E',
+    Gray2: '#BDBDBD',
+    Gray3: '#D9D9D9',
+    Gray4: '#EEEEEE',
+    Gray5: '#F9F9F9',
+    Black: '#313131',
+  },
+};
