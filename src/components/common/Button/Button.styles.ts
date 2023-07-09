6829bcaf7c350e8cf2365925349e4c0ffba16970
kweonsikyung <ekrk6632@naver.com>
2023-07-09 22:43:44 +0900

    add: Button Components

---
+++ src/components/common/Button/Button.styles.ts
@@ -0,0 +1,19 @@
+import { COLORS } from '../../../styles/colors';
+import styled from '@emotion/styled';
+
+// 가독성을 위해 스타일 파일은 별도로 둡니다.
+
+interface ButtonProps {
+  width?: number;
+  height?: number;
+}
+
+export const Button = styled.div<ButtonProps>`
+  width: ${(props) => (props.width ? `${props.width}px` : '358px')};
+  height: ${(props) => (props.height ? `${props.height}px` : '56px')};
+  line-height: ${(props) => (props.height ? `${props.height}px` : '56px')};
+  border-radius: 12px;
+  text-align: center;
+  color: ${COLORS.grayscale.white};
+  background-color: ${COLORS.SSU.blue};
+`;
