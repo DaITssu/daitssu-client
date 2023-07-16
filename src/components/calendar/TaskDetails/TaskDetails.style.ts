6e95124964eefd4d8b1b6deddae507f7e6e49f0c
halfmoon-mind <5338095@gmail.com>
2023-07-17 02:18:32 +0900

    feat: 사용하지 않는 스타일 제거

--- src/components/calendar/TaskDetails/TaskDetails.style.ts
+++ src/components/calendar/TaskDetails/TaskDetails.style.ts
@@ -20,27 +20,6 @@ export const Titles = styled.div`
   flex-direction: column;
 `;
 
-export const Title = styled.p`
-  color: #000000;
-  font-size: 16px;
-  margin: 0px;
-`;
-
-export const dateLabel = styled.p`
-  color: #6e6e6e;
-  font-size: 14px;
-  margin: 0px;
-`;
-
-export const finishButton = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  padding: 3px 10px;
-  border-radius: 12px;
-  background: var(--error-color, #f36262);
-  color: #ffffff;
-  font-size: 12px;
-  line-break: strict;
+export const Label = styled.p`
+  margin: 0;
 `;
