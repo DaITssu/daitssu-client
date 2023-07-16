a2352df72c1db94531117200d771b4b6365c9d64
halfmoon-mind <5338095@gmail.com>
2023-07-17 02:20:02 +0900

    feat: 사용되는 임시 DTO 추가 Task, Subject

---
+++ src/types/Subject.ts
@@ -0,0 +1,34 @@
+import { TaskDTO } from './Task';
+
+export interface SubjectDTO {
+  id: number;
+  title: string;
+  color: string;
+  tasks: TaskDTO[];
+}
+
+export interface SubjectProps {
+  subject: SubjectDTO;
+}
+
+export const SubjectExample: SubjectDTO = {
+  id: 1,
+  title: '웹프로그래밍실습',
+  color: '#FF0000',
+  tasks: [
+    {
+      id: 1,
+      title: '과제1',
+      dueDate: '2021-10-01',
+      isFinished: false,
+      type: 'assignment',
+    },
+    {
+      id: 2,
+      title: '과제2',
+      dueDate: '2021-10-02',
+      isFinished: false,
+      type: 'assignment',
+    },
+  ],
+};
