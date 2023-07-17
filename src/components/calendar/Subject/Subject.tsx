730d7f6bd26ad5ad6d08738eb6482ccc4a6a4a7e
halfmoon-mind <5338095@gmail.com>
2023-07-17 14:26:22 +0900

    feat: 이름 변경점 반영

--- src/components/calendar/Subject/Subject.tsx
+++ src/components/calendar/Subject/Subject.tsx
@@ -1,15 +1,14 @@
-import { TaskDTO, TaskProps } from '@/types/Task';
 import * as styles from './Subject.style';
-import TaskDetails from '../TaskDetails';
-import SubjectDetails from '../SubjectDetails';
+import SubjectContents from '../SubjectContents';
+import SubjectTitle from '../SubjectTitle';
 import { SubjectProps } from '@/types/Subject';
 
 const Subject = ({ subject }: SubjectProps) => (
   <>
-    <SubjectDetails subject={subject}></SubjectDetails>
+    <SubjectTitle subject={subject}></SubjectTitle>
     <styles.LeftDiv>
       {subject.tasks.map((task, index) => (
-        <TaskDetails key={index} task={task} />
+        <SubjectContents key={index} task={task} />
       ))}
     </styles.LeftDiv>
   </>
--- src/pages/calendar.page.tsx
+++ src/pages/calendar.page.tsx
@@ -1,17 +1,10 @@
-import TaskDetails from '@/components/calendar/TaskDetails';
+import SubjectList from '@/components/calendar/SubjectList';
+import { SubjectDTOExample } from '@/types/Subject';
 
 export default function Calendar() {
   return (
     <div>
-      <TaskDetails
-        task={{
-          id: 1,
-          title: 'test',
-          dueDate: '2022-10-10T09:00:00.000Z',
-          isFinished: false,
-          type: 'test',
-        }}
-      />
+      <SubjectList subjectList={[SubjectDTOExample, SubjectDTOExample]} />
     </div>
   );
 }
