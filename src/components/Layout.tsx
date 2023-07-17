d4be35f6d735ff4b5f38c76e65246654ce3238b4
JooHui-void <69834729+JooHui-void@users.noreply.github.com>
2023-07-17 19:54:11 +0900

    add notice-list, tab (#18)

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import TopBar from "./TopBar";
+import TopTab from "./TopTab";
+import NoticeList from "./NoticeList";
+import {useEffect} from "react";
+
+
+const Layout = ()=>{
+  return(
+    <div>
+      
+      <TopBar/>
+      <TopTab/>
+    </div>
+    
+  );
+};
+export default Layout;
