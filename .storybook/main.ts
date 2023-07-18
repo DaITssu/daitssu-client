a6f4dcd12f2780a2a3a20b98463371c3bff17da3
JjungminLee <julie0964@naver.com>
2023-07-18 23:04:20 +0900

    fix:storybook 절대경로 문제 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -13,5 +16,28 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
+  webpackFinal: (config) => {
+    config.resolve = config.resolve || {};
+
+    config.resolve.modules = [
+      path.resolve(__dirname, '..'),
+      'node_modules',
+      'styles',
+    ];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@assets': path.resolve(__dirname, '../src/assets'),
+    };
+
+    // tsconfig-paths-webpack-plugin 설정 추가를 위해 배열로 초기화합니다.
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin(),
+    ];
+
+    return config;
+  },
 };
+
 export default config;
