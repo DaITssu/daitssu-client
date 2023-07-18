beb56ba06dafb8c5af6467dc670b3603e66ab3f2
kweonsikyung <ekrk6632@naver.com>
2023-07-19 02:24:42 +0900

    fix: letterSpacing 관련 수정

--- src/styles/constants/textStyles.ts
+++ src/styles/constants/textStyles.ts
@@ -3,77 +3,77 @@ export const TEXT_STYLES = {
   NavM10: {
     fontSize: 10,
     fontWeight: 500,
-    letterSpacing: -0.11,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Caption R 12
   CapR12: {
     fontSize: 12,
     fontWeight: 400,
-    letterSpacing: -0.132,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Caption M 12
   CapM12: {
     fontSize: 12,
     fontWeight: 500,
-    letterSpacing: -0.132,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Caption R 14
   CapR14: {
     fontSize: 14,
     fontWeight: 400,
-    letterSpacing: -0.154,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Caption M 14
   CapM14: {
     fontSize: 14,
     fontWeight: 500,
-    letterSpacing: -0.154,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Body R 16
   BodyR16: {
     fontSize: 16,
     fontWeight: 400,
-    letterSpacing: -0.176,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Body M 16
   BodyM16: {
     fontSize: 16,
     fontWeight: 500,
-    letterSpacing: -0.176,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Head M 20
   HeadM20: {
     fontSize: 14,
     fontWeight: 400,
-    letterSpacing: -0.154,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Head SM 20
   HeadSM20: {
     fontSize: 20,
     fontWeight: 600,
-    letterSpacing: -0.22,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Button M 18
   BtnM18: {
     fontSize: 18,
     fontWeight: 500,
-    letterSpacing: -0.198,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
   //Button SB 18
   BtnSB18: {
     fontSize: 18,
     fontWeight: 600,
-    letterSpacing: -0.198,
+    letterSpacing: '-1.1%',
     fontFamily: 'Pretendard',
   },
 };
