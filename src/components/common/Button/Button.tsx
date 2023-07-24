ba0e0ee67b90d1a3c8b7655790179408bad0d35b
ChoiSangwon <fdsa200@naver.com>
2023-07-24 18:48:38 +0900

    feat : 신고 Button style 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -4,11 +4,12 @@ interface ButtonProps {
   label?: string;
   width?: number;
   height?: number;
+  color?: string;
   onClick?: () => void;
 }
 
-const Button = ({ label, width, height, onClick }: ButtonProps) => (
-  <styles.Button width={width} height={height} onClick={onClick}>
+const Button = ({ label, width, height, color, onClick }: ButtonProps) => (
+  <styles.Button width={width} height={height} color={color} onClick={onClick}>
     {label}
   </styles.Button>
 );
