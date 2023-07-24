5c67d045d5d0c4711e516f43f7b576188e43f849
SuJin <kimsujin001127@gmail.com>
2023-07-24 16:21:05 +0900

    feat: profile 화면 구성 완료

--- src/components/community/info/CommunityInfo.tsx
+++ src/components/community/info/CommunityInfo.tsx
@@ -28,41 +28,59 @@ const CommunityInfo = () => {
       <InfoBox>
         {/** TODO: 태그 형식으로 변경 필요 */}
         <div>질문</div>
-      </InfoBox>
-      <ProfileBox>
-        <ProfileLeftBox></ProfileLeftBox>
-        <ProfileCenterBox>
-          <ProfileTopBox>
-            <NickNameBox></NickNameBox>
-          </ProfileTopBox>
-          <ProfileBottomBox>
-            <DateBox></DateBox>
-            <TimeBox></TimeBox>
-          </ProfileBottomBox>
-        </ProfileCenterBox>
-        <ProfileRightBox>
-          <ViewBox>
-            <ViewIconBox></ViewIconBox>
-            <ViewCountBox></ViewCountBox>
-          </ViewBox>
-        </ProfileRightBox>
-      </ProfileBox>
-      <TitleBox></TitleBox>
-      <ContentBox></ContentBox>
-      <UnderBarBox>
-        <LikesBox>
-          <IconBox></IconBox>
-          <CountBox></CountBox>
-        </LikesBox>
-        <CommentBox>
-          <IconBox></IconBox>
-          <CountBox></CountBox>
-        </CommentBox>
-        <BookmarkBox>
-          <IconBox></IconBox>
-          <CountBox></CountBox>
-        </BookmarkBox>
-      </UnderBarBox>
+      </InfoBox>{' '}
+      <Padding>
+        <ProfileBox>
+          <ProfileLeftBox>
+            <Image
+              src="/noticeInfo/profile.svg"
+              alt="profile image"
+              width={45}
+              height={45}
+              priority
+            />
+          </ProfileLeftBox>
+          <ProfileCenterBox>
+            <ProfileTopBox>
+              <NickNameBox>닉네임</NickNameBox>
+            </ProfileTopBox>
+            <ProfileBottomBox>
+              <DateBox>2023/05/20</DateBox>
+              <TimeBox>21:23</TimeBox>
+              <ProfileRightBox>
+                <ViewBox>
+                  <ViewIconBox>
+                    <Image
+                      src="/noticeInfo/watch.svg"
+                      alt="watch count"
+                      width={17}
+                      height={17}
+                      priority
+                    />
+                  </ViewIconBox>
+                  <ViewCountBox>5회</ViewCountBox>
+                </ViewBox>
+              </ProfileRightBox>
+            </ProfileBottomBox>
+          </ProfileCenterBox>
+        </ProfileBox>
+        <TitleBox></TitleBox>
+        <ContentBox></ContentBox>
+        <UnderBarBox>
+          <LikesBox>
+            <IconBox></IconBox>
+            <CountBox></CountBox>
+          </LikesBox>
+          <CommentBox>
+            <IconBox></IconBox>
+            <CountBox></CountBox>
+          </CommentBox>
+          <BookmarkBox>
+            <IconBox></IconBox>
+            <CountBox></CountBox>
+          </BookmarkBox>
+        </UnderBarBox>
+      </Padding>
       <BottomBox>
         <CommentTitleBox>댓글</CommentTitleBox>
       </BottomBox>
@@ -77,6 +95,10 @@ const Container = styled.div`
   hr {
     border: solid 1px rgba(217, 217, 217, 1);
   }
+`;
+
+const Padding = styled.div`
+  padding: 0 10px;
 `;
 
 const MenuBox = styled.div`
@@ -105,29 +127,71 @@ const InfoBox = styled.div`
   padding: 10px;
 `;
 
-const ProfileBox = styled.div``;
+const ProfileBox = styled.div`
+  display: flex;
+`;
 
 const ProfileLeftBox = styled.div``;
 
-const ProfileCenterBox = styled.div``;
+const ProfileCenterBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto 0;
+  width: 345px;
+`;
 
-const ProfileRightBox = styled.div``;
+const ProfileRightBox = styled.div`
+  display: flex;
+  margin-left: auto;
+`;
 
 const ProfileTopBox = styled.div``;
 
-const NickNameBox = styled.div``;
+const NickNameBox = styled.div`
+  display: flex;
+  font-size: 16px;
+  font-weight: 500;
+  align-items: center;
+  margin-left: 10px;
+`;
 
-const ProfileBottomBox = styled.div``;
+const ProfileBottomBox = styled.div`
+  display: flex;
+  height: 24px;
+  align-items: center;
+`;
 
-const DateBox = styled.div``;
+const DateBox = styled.div`
+  margin-left: 10px;
+  font-size: 14px;
+  align-items: center;
+  color: #6e6e6e;
+  display: flex;
+`;
 
-const TimeBox = styled.div``;
+const TimeBox = styled.div`
+  display: flex;
+  font-size: 14px;
+  align-items: center;
+  margin-left: 7px;
+  color: #6e6e6e;
+`;
 
-const ViewBox = styled.div``;
+const ViewBox = styled.div`
+  margin-left: auto;
+  display: flex;
+`;
 
-const ViewIconBox = styled.div``;
+const ViewIconBox = styled.div`
+  align-items: center;
+`;
 
-const ViewCountBox = styled.div``;
+const ViewCountBox = styled.div`
+  margin-left: 6px;
+  font-size: 14px;
+  align-items: center;
+  color: #6e6e6e;
+`;
 
 const TitleBox = styled.div``;
 
