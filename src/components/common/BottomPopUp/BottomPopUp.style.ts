fa3a7889c71a3387be5b9a226db3fbcb40762f9c
ChoiSangwon <fdsa200@naver.com>
2023-07-24 19:32:36 +0900

    fix : BottomPopUp padding값 변경

--- src/components/common/BottomPopUp/BottomPopUp.style.ts
+++ src/components/common/BottomPopUp/BottomPopUp.style.ts
@@ -1,15 +1,15 @@
-import { COLORS } from '@/styles/constants/colors';
+import { COLORS } from '../../../styles/constants/colors';
 import styled from '@emotion/styled';
 
 export const BottomPopUp = styled.div`
   position: fixed;
-  top: 90%;
+  top: 80%;
   left: 50%;
   transform: translate(-50%, -50%);
-  padding: 20px;
   background: ${COLORS.SSU.secondary};
   width: 358px;
-  height: 23px;
+  height: 53px;
+  padding-left: 20px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
   z-index: 9999;
   border-radius: 15px;
