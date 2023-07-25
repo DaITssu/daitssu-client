69c25b95b8d76dada153c62307a04205a100ede1
SuJin <kimsujin001127@gmail.com>
2023-07-25 18:51:52 +0900

    refactor: style 파일 분리

---
+++ src/components/common/Comment/CommentInput/CommentInput.styles.ts
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+export const MyCommentBox = styled.div`
+  margin: auto;
+  width: 358px;
+  height: 45px;
+`;
+
+export const CommentFlexBox = styled.div`
+  position: fixed;
+  top: 90%;
+  display: flex;
+  width: 358px;
+`;
+
+export const MyLeftBox = styled.div`
+  margin-right: auto;
+`;
+
+export const MyRightBox = styled.div`
+  margin-left: auto;
+`;
+
+export const MyCommentLabel = styled.label`
+  input {
+    background-color: rgba(238, 238, 238, 1);
+    width: 293px;
+    height: 45px;
+    border-radius: 10px;
+    border: none;
+  }
+  input::placeholder {
+    color: rgba(110, 110, 110, 1);
+    font-weight: 500;
+    font-size: 16px;
+  }
+`;
+
+export const MyCommentBtn = styled.button``;
--- src/components/common/Comment/CommentInput/CommentInput.tsx
+++ src/components/common/Comment/CommentInput/CommentInput.tsx
@@ -1,59 +1,21 @@
-import styled from '@emotion/styled';
+import * as styles from './CommentInput.styles';
 
 function CommentInput() {
   return (
-    <MyCommentBox>
-      <CommentFlexBox>
-        <MyLeftBox>
-          <MyCommentLabel>
+    <styles.MyCommentBox>
+      <styles.CommentFlexBox>
+        <styles.MyLeftBox>
+          <styles.MyCommentLabel>
             <input placeholder="  댓글을 남겨보세요."></input>
-          </MyCommentLabel>
-        </MyLeftBox>
-        <MyRightBox>
+          </styles.MyCommentLabel>
+        </styles.MyLeftBox>
+        <styles.MyRightBox>
           {/** TODO: common Btn으로 디자인 변경하기 */}
-          <MyCommentBtn>등록</MyCommentBtn>
-        </MyRightBox>
-      </CommentFlexBox>
-    </MyCommentBox>
+          <styles.MyCommentBtn>등록</styles.MyCommentBtn>
+        </styles.MyRightBox>
+      </styles.CommentFlexBox>
+    </styles.MyCommentBox>
   );
 }
-
-const MyCommentBox = styled.div`
-  margin: auto;
-  width: 358px;
-  height: 45px;
-`;
-
-const CommentFlexBox = styled.div`
-  position: fixed;
-  top: 90%;
-  display: flex;
-  width: 358px;
-`;
-
-const MyLeftBox = styled.div`
-  margin-right: auto;
-`;
-
-const MyRightBox = styled.div`
-  margin-left: auto;
-`;
-
-const MyCommentLabel = styled.label`
-  input {
-    background-color: rgba(238, 238, 238, 1);
-    width: 293px;
-    height: 45px;
-    border-radius: 10px;
-    border: none;
-  }
-  input::placeholder {
-    color: rgba(110, 110, 110, 1);
-    font-weight: 500;
-    font-size: 16px;
-  }
-`;
-
-const MyCommentBtn = styled.button``;
 
 export default CommentInput;
