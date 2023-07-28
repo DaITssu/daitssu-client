11d78f5cf40464369536fbfae9556d7470a3954a
JooHui-void <elis100@naver.com>
2023-07-28 23:41:17 +0900

    feat : emotion css파일

---
+++ src/styles/community/list/CommunityList.style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const CommunityListBox = styled.div
+`
+position : absolute;
+width:390px;
+height:670px;
+top : 166px;
+overflow : auto;
+-ms-overflow-style: none;
+scrollbar-width: none;
+::-webkit-scrollbar {
+  display: none;
+}
+`;
