911eeda22a93e78814342555c7c273120f289428
JooHui-void <elis100@naver.com>
2023-07-28 23:39:45 +0900

    feat : 커뮤니티 리스트 제작

---
+++ src/components/community/list/CommunityList.tsx
@@ -0,0 +1,48 @@
+
+import styled from '@emotion/styled';
+import * as styles from '../../../styles/community/list/CommunityList.style';
+import React, {FC, ChangeEvent, useState, useEffect} from 'react';
+import CommunityItem, {CommunityItemProps} from './CommunityListItem';
+
+const CommunityList:FC = ()=>{
+
+const [mounted, setMounted] = useState<boolean>(false); //기본 브라우저의 클라이언트 사이드 렌더링 제거 
+
+  useEffect(() => {
+    setMounted(true);
+  },[]);
+
+  const [items,setItems] = useState<CommunityItemProps[]>(
+    [{bundle:"잡담",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요~~~~~~~~~~~~~~~~?\n",
+      minutes : 3, review : 5},
+    {bundle:"asdfasdf",title : "장학금 언제 줘요~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n",minutes : 3, review : 999},
+    {bundle:"aaaaaa",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n",minutes : 3, review : 5},
+    {bundle:"잡담",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n",minutes : 3, review : 5},
+    {bundle:"잡담",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n",minutes : 3, review : 5},
+    {bundle:"잡담",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n",minutes : 3, review : 5},
+    {bundle:"잡담",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n",minutes : 3, review : 5},
+    {bundle:"잡담",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n",minutes : 3, review : 5},],
+
+    
+  );
+  
+  
+  return(
+    mounted&&
+    <>
+      <styles.CommunityListBox>
+        
+        {items.map((item:CommunityItemProps,key:number)=> {
+          return <CommunityItem
+          key = {key}
+          item = {item}/>;
+        })}
+      </styles.CommunityListBox>
+     
+    </>
+    
+  );
+};
+
+
+export default CommunityList;
