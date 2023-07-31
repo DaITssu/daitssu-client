c9f34b30de5e08d8b79068b14c631cbe70e82498
JooHui-void <elis100@naver.com>
2023-07-31 21:08:29 +0900

    feat : 커뮤니티 태그 목록

--- src/components/community/list/CommunityList.tsx
+++ src/components/community/list/CommunityList.tsx
@@ -3,6 +3,9 @@ import styled from '@emotion/styled';
 import * as styles from '../../../styles/community/list/CommunityList.style';
 import React, {FC, ChangeEvent, useState, useEffect} from 'react';
 import CommunityItem, {CommunityItemProps} from './CommunityListItem';
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react';
+
 
 const CommunityList:FC = ()=>{
 
@@ -11,6 +14,19 @@ const [mounted, setMounted] = useState<boolean>(false); //기본 브라우저의
   useEffect(() => {
     setMounted(true);
   },[]);
+
+  const [index,setIndex] = useState(0);
+
+  const selectTagHandler = (n : number) =>{
+    setIndex(n);
+    //태그 필터링 목록으로 변환 이벤트 추가 
+  }
+
+  const plus_button_onClick = () =>{
+    // 커뮤니티 게시글 추가 컴포넌트 연결 
+  }
+  
+  const tag = ["전체", "인기글", "잡담", "질문", "정보"];
 
   const [items,setItems] = useState<CommunityItemProps[]>(
     [{bundle:"잡담",title : "장학금 언제 줘요?",contents : "장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요?\n장학금 언제 줘요~~~~~~~~~~~~~~~~?\n",
@@ -30,6 +46,17 @@ const [mounted, setMounted] = useState<boolean>(false); //기본 브라우저의
   return(
     mounted&&
     <>
+      <styles.TagList>
+        {tag.map((name, idx) =>(
+          <styles.TagButton
+          css={[
+            index === idx
+            ? css`background-color: #5EBEEB;color: #FFFFFF;` // 선택됨
+            : css`background-color: #FFFFFF;color: #5EBEEB;`,// 선택안됨
+          ]}
+          onClick={()=> selectTagHandler(idx)}>{name}</styles.TagButton>
+        ))}
+      </styles.TagList>
       <styles.CommunityListBox>
         
         {items.map((item:CommunityItemProps,key:number)=> {
@@ -38,7 +65,7 @@ const [mounted, setMounted] = useState<boolean>(false); //기본 브라우저의
           item = {item}/>;
         })}
       </styles.CommunityListBox>
-      <styles.PlusButton src="/assets/icon/Button/plus_button.svg"/>
+      <styles.PlusButton src="/assets/icon/Button/plus_button.svg" onClick = {plus_button_onClick}/>
     </>
     
   );
