c84e7897108efd1088d30a45850d6bc7cd55d4ed
Sanghyeon Sim <5338095@gmail.com>
2023-07-31 14:41:31 +0900

    feat: 캘린더 전체 뷰 제작

---
+++ src/components/Calendar/Calendar/Calendar.stories.tsx
@@ -0,0 +1,11 @@
+// Calendar.stories.tsx
+
+import React from 'react';
+import PageCalendar from './Page_Calendar';
+export default {
+  title: 'PageCalendar',
+  component: PageCalendar,
+};
+
+export const PrimaryPageCalendars = () => <PageCalendar />;
+PrimaryPageCalendars.storyName = 'page Calendar';
