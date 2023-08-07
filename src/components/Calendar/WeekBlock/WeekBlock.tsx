91ef54ac4133010472d8fb6a917e440c7eecd291
Sanghyeon Sim <5338095@gmail.com>
2023-08-07 15:24:01 +0900

    refactor: 요일 block 네이밍 및 컨벤션 적용

--- src/components/Calendar/WeekBlock/WeekBlock.tsx
+++ src/components/Calendar/WeekBlock/WeekBlock.tsx
@@ -1,39 +1,29 @@
-
 import React from 'react';
 import { Container } from '../Calendar/Calendar.styles';
-import { DayBlockN,TasksToday,DayBlockBox,Task } from '../DayBlock/DayBlock.styles';
+import { DayBlockN, DayBlockBox } from '../DayBlock/DayBlock.styles';
 
 const WeekBlock = () => {
-  const whatDay: string[] = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
+  const dayList: string[] = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
   return (
-    <>
-      <Container>
-        <div className="item" style={{ height: '29px' }}>
-          <DayBlockBox>
-            <DayBlockN SpecialDay={0}
-              style={{ fontSize: '14px', height: '29px' }}>
-              {'SUN'}
-            </DayBlockN>
-          </DayBlockBox>
-        </div>
-        {whatDay.map((wd, index) => (
-          <div style={{ height: '29px' }}  key={`weekday-${index}`}>
-            <DayBlockBox>
-              <DayBlockN FontSize='14px' Height='29px' key={`weekday-${index}`}>
-                {wd}
-              </DayBlockN>
-            </DayBlockBox>
-          </div>
-        ))}
-        <div className="item" style={{ height: '29px' }}>
-          <DayBlockBox>
-            <DayBlockN SpecialDay={6} FontSize='14px' Height='29px' key={`weekday-${6}}`}>
-                {"SAT"}
-              </DayBlockN>
-          </DayBlockBox>
-        </div>
-      </Container>
-    </>
+    <Container>
+      <DayBlockBox>
+        <DayBlockN weekday={0} style={{ fontSize: '14px', height: '29px' }}>
+          {'SUN'}
+        </DayBlockN>
+      </DayBlockBox>
+      {dayList.map((wd, index) => (
+        <DayBlockBox key={`weekday-${index}`}>
+          <DayBlockN style={{ fontSize: '14px', height: '29px' }}>
+            {wd}
+          </DayBlockN>
+        </DayBlockBox>
+      ))}
+      <DayBlockBox>
+        <DayBlockN weekday={6} style={{ fontSize: '14px', height: '29px' }}>
+          {'SAT'}
+        </DayBlockN>
+      </DayBlockBox>
+    </Container>
   );
 };
 
