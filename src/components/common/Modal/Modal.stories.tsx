0f5a276314f9dc2137b6032dd7e31786d0a30f15
Sanghyeon Sim <5338095@gmail.com>
2023-08-08 13:46:46 +0900

    feat: 모달 기본 컴포넌트

---
+++ src/components/common/Modal/Modal.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Modal from './Modal';
+import useModal from './useModal';
+
+const meta: Meta<typeof Modal> = {
+  title: 'Modal',
+  component: Modal,
+};
+
+export default meta;
+type Story = StoryObj<typeof Modal>;
+
+export const ModalExample: Story = () => {
+  const { open, PopUp, close } = useModal();
+
+  return (
+    <div>
+      <button onClick={open}>Open Modal</button>
+      <PopUp>
+        <Modal onClose={close} title="This is Title">
+          <div>MODAL_EXAMPLE</div>
+        </Modal>
+      </PopUp>
+    </div>
+  );
+};
+
+ModalExample.args = {
+  // isOpen: false,
+  // children: <div>Modal</div>,
+};
