79fa0f4017db00df1754343dc3ae82b9a9447574
Sanghyeon Sim <5338095@gmail.com>
2023-08-30 01:39:01 +0900

    feat: 스마트캠퍼스 캘린더 페이지 뷰 생성

--- src/pages/calendar.page.tsx
+++ src/pages/calendar.page.tsx
@@ -1,10 +1,27 @@
-import SubjectList from '@/components/calendar/SubjectList';
-import { SubjectDTOExample } from '@/types/Subject';
+import { useState } from 'react';
+import CalendarScreen from '@/components/calendar/CalendarScreen';
+import useModal from '@/components/common/Modal/useModal';
 
 export default function Calendar() {
+  const [year, setYear] = useState<number>(2023);
+  const [month, setMonth] = useState<number>(8);
+  const [selectDay, setSelectDay] = useState<number>(23);
+
   return (
     <div>
-      <SubjectList subjectList={[SubjectDTOExample, SubjectDTOExample]} />
+      <CalendarScreen
+        year={year}
+        month={month}
+        selectDay={selectDay}
+        dayTasks={{
+          3: ['#ffaa00', '#ff0000', '#ffaa00', '#ffaa00'],
+          5: ['#ffaa00', '#ff0000'],
+          14: ['#ffaa00', '#ff0000'],
+        }}
+        onDayClick={(day: number) => {
+          setSelectDay(day);
+        }}
+      />
     </div>
   );
 }
