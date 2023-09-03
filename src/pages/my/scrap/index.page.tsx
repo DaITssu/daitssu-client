cd4fc61061d403ef02c9964d5bcb6274deb96e95
SuJin <kimsujin001127@gmail.com>
2023-09-03 18:15:20 +0900

    feat: Header. Nav 바 적용

--- src/pages/my/scrap/index.page.tsx
+++ src/pages/my/scrap/index.page.tsx
@@ -1,11 +1,16 @@
+import Header from '@/components/common/Header/Header';
+import NavigationBar from '@/components/common/Navbar/NavigationBar';
 import ScrapList from '@/components/myPage/Scrap/ScrapList';
 
 export default function Home() {
   return (
     <>
+      <Header />
+
       <ScrapList />
       <ScrapList />
       <ScrapList />
+      <NavigationBar focusType={'MY_PAGE'} />
     </>
   );
 }
