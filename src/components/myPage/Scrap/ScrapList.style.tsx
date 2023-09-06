4be15ca1e82611a0abc3232465a3be90a036538e
SuJin <kimsujin001127@gmail.com>
2023-09-06 09:23:59 +0900

    [#50] 마이페이지 스크랩 페이지 (#60)
    * feat: 기본 컴포넌트 구성
    
    * feat: style 작업
    
    * feat: storybook 추가
    
    * fix: 이미지 변경
    
    * feat: Header. Nav 바 적용

---
+++ src/components/myPage/Scrap/ScrapList.style.tsx
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled';
+
+export const FlexBox = styled.div`
+  display: flex;
+  background-color: white;
+  width: 100%;
+  border-bottom: 1px solid #d9d9d9;
+  padding-top: 0.8rem;
+`;
+
+export const LeftBox = styled.div``;
+
+export const RightBox = styled.div`
+  width: 340px;
+`;
+
+export const TopBox = styled.div`
+  display: flex;
+  margin-bottom: 0.6rem;
+`;
+
+export const CheckBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 35px;
+  width: 35px;
+`;
+
+export const TagBox = styled.div``;
+
+export const TimeBox = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  color: #6e6e6e;
+  margin-left: auto;
+`;
+
+export const TitleBox = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const BottomBox = styled.div`
+  display: flex;
+  margin-bottom: 0.8rem;
+`;
+
+export const ContentBox = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  color: #6e6e6e;
+  margin-top: 0.6rem;
+  padding-right: 1rem;
+`;
+
+export const MessageBox = styled.div`
+  display: flex;
+  margin-top: auto;
+`;
+
+export const MessageIconBox = styled.div``;
+
+export const MessageCntBox = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  color: #6e6e6e;
+  margin-left: 0.5rem;
+`;
---
+++ src/components/myPage/Scrap/ScrapList.tsx
@@ -0,0 +1,51 @@
+import Category from '@/components/common/Category';
+import * as styles from '@/components/myPage/Scrap/ScrapList.style';
+import Image from 'next/image';
+import DefaultCheckBox from '@icons/icon/CheckBox/DefaultCheckBox.svg';
+import Comment from '@icons/icon/Icon24/Comment.svg';
+
+const ScrapList = () => {
+  return (
+    <styles.FlexBox>
+      <styles.LeftBox>
+        <styles.CheckBox>
+          <Image
+            src={DefaultCheckBox}
+            alt="check"
+            width={15}
+            height={15}
+            priority
+          />
+        </styles.CheckBox>
+      </styles.LeftBox>
+      <styles.RightBox>
+        <styles.TopBox>
+          <styles.TagBox>
+            <Category label="잡담" BgColor={false} />
+          </styles.TagBox>
+          <styles.TimeBox>3분 전</styles.TimeBox>
+        </styles.TopBox>
+        <styles.TitleBox>서점에</styles.TitleBox>
+        <styles.BottomBox>
+          <styles.ContentBox>
+            서점에 이 책도 파나요 서점에 이 책도 파나요 서점에 이 책도 파나요
+          </styles.ContentBox>
+          <styles.MessageBox>
+            <styles.MessageIconBox>
+              <Image
+                src={Comment}
+                alt="comment"
+                width={15}
+                height={15}
+                priority
+              />
+            </styles.MessageIconBox>
+            <styles.MessageCntBox>5</styles.MessageCntBox>
+          </styles.MessageBox>
+        </styles.BottomBox>
+      </styles.RightBox>
+    </styles.FlexBox>
+  );
+};
+
+export default ScrapList;
