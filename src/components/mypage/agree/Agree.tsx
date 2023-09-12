135cef47d131f5de2a8e8c6592f65c9a019b5dc2
SuJin <kimsujin001127@gmail.com>
2023-09-12 16:28:32 +0900

    [#62] 약관동의 페이지 (#66)
    * feat: 약관동의 페이지
    
    * feat: 헤더, nav 추가
    
    * feat: 전체선택 기능
    
    * feat: storybook 추가
    
    * fix: 네비게이션바 밑으로 고정
    
    * style: 완료 버튼 맨 밑으로 내리기

---
+++ src/components/mypage/agree/Agree.tsx
@@ -0,0 +1,196 @@
+import Button from '@/components/common/Button';
+import * as styles from './Agree.style';
+import AgreeText from './AgreeText';
+import NavigationBar from '@/components/common/Navbar/NavigationBar';
+import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { userNavAtom } from '@/states/userNavAtom';
+
+import Image from 'next/image';
+import DefaultRadio from '@icons/icon/Radio/DefaultRadio.svg';
+import CheckedRadio from '@icons/icon/Radio/CheckedRadio.svg';
+
+const Agree = () => {
+  const [agreeArray, setAgreeArray] = useState([false, false, false, false]);
+  const activeNavType = useRecoilValue(userNavAtom).activeNavType;
+
+  const handleOnClickCheck = (idx: number) => {
+    if (idx === 0) {
+      setAgreeArray([
+        !agreeArray[0],
+        !agreeArray[0],
+        !agreeArray[0],
+        !agreeArray[0],
+      ]);
+    } else {
+      const updatedArray = [...agreeArray];
+      updatedArray[idx] = !updatedArray[idx];
+      setAgreeArray(updatedArray);
+    }
+  };
+
+  return (
+    <>
+      <div style={{ height: '46px' }}> 헤더자리</div>
+      <styles.FlexBox>
+        <styles.TopBox>
+          <styles.ContainCheckBox>
+            <styles.CheckBox
+              onClick={() => {
+                handleOnClickCheck(0);
+              }}
+            >
+              {agreeArray[0] ? (
+                <Image
+                  src={CheckedRadio}
+                  alt="check"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              ) : (
+                <Image
+                  src={DefaultRadio}
+                  alt="uncheck"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              )}
+            </styles.CheckBox>
+
+            <AgreeText
+              type="none"
+              text="약관 전체 동의 (선택사항 모두 포함)"
+              arrow={false}
+              AllAgree={false}
+              check={agreeArray[0]}
+              agreeArray={agreeArray}
+              setAgreeArray={setAgreeArray}
+            />
+          </styles.ContainCheckBox>
+        </styles.TopBox>
+        <styles.MiddleBox />
+        <styles.BottomBox>
+          <styles.ContainCheckBox>
+            <styles.CheckBox
+              onClick={() => {
+                handleOnClickCheck(1);
+              }}
+            >
+              {agreeArray[1] ? (
+                <Image
+                  src={CheckedRadio}
+                  alt="check"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              ) : (
+                <Image
+                  src={DefaultRadio}
+                  alt="uncheck"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              )}
+            </styles.CheckBox>
+
+            <AgreeText
+              type="compulsory"
+              text="다잇슈 이용약관"
+              arrow={true}
+              check={agreeArray[1]}
+              agreeArray={agreeArray}
+              setAgreeArray={setAgreeArray}
+            />
+          </styles.ContainCheckBox>
+          <styles.ContainCheckBox>
+            <styles.CheckBox
+              onClick={() => {
+                handleOnClickCheck(2);
+              }}
+            >
+              {agreeArray[2] ? (
+                <Image
+                  src={CheckedRadio}
+                  alt="check"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              ) : (
+                <Image
+                  src={DefaultRadio}
+                  alt="uncheck"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              )}
+            </styles.CheckBox>
+
+            <AgreeText
+              type="compulsory"
+              text="개인정보 수집 및 이용약관"
+              arrow={true}
+              check={agreeArray[2]}
+              agreeArray={agreeArray}
+              setAgreeArray={setAgreeArray}
+            />
+          </styles.ContainCheckBox>
+
+          <styles.ContainCheckBox>
+            <styles.CheckBox
+              onClick={() => {
+                handleOnClickCheck(3);
+              }}
+            >
+              {agreeArray[3] ? (
+                <Image
+                  src={CheckedRadio}
+                  alt="check"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              ) : (
+                <Image
+                  src={DefaultRadio}
+                  alt="uncheck"
+                  width={18}
+                  height={18}
+                  priority
+                />
+              )}
+            </styles.CheckBox>
+
+            <AgreeText
+              type="select"
+              text="제3자 정보 제공 동의"
+              arrow={true}
+              check={agreeArray[3]}
+              agreeArray={agreeArray}
+              setAgreeArray={setAgreeArray}
+            />
+          </styles.ContainCheckBox>
+        </styles.BottomBox>
+        <styles.InfoBox>
+          <p>회원님께서는 동의를 거부할 수 있습니다.</p>
+          <p>단, 필수항목 동의 거부 시에는 회원가입이 제한됩니다.</p>
+          <div style={{ height: '20px' }}></div>
+          <p>약관동의 후 해당 서비스 사용이 가능합니다.</p>
+        </styles.InfoBox>
+        <styles.FinishButtonBox>
+          <Button label="완료" width={358} />
+        </styles.FinishButtonBox>
+      </styles.FlexBox>
+      <styles.NavBarWrapper>
+        <NavigationBar focusType={activeNavType} />
+      </styles.NavBarWrapper>
+    </>
+  );
+};
+
+export default Agree;
