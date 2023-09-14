b4402651d7f446ad553c022213772919ef2beaea
jwo0o0 <jwo0o0358@gmail.com>
2023-09-14 22:54:36 +0900

    feat: 랜딩 뷰 로고 컴포넌트

---
+++ src/components/landing/LandingLogo/LandingLogo.style.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { COLORS } from '@/styles/constants/colors';
+
+export const LogoContainer = styled.div`
+  position: absolute;
+  top: calc(100vw / 2.5);
+  left: 36px;
+`;
+
+export const LogoImg = styled.img`
+  width: 70%;
+`;
