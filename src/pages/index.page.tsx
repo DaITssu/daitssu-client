5c4b3c041558e070ab4d863f7a9766e4b7abb413
jwo0o0 <jwo0o0358@gmail.com>
2023-09-14 22:54:13 +0900

    feat: 로그인 여부에 따라 랜딩 화면 노출

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,11 @@
 import MainLayout from './layout';
+import { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { userNavAtom, IUserNavAtom } from '@/states/userNavAtom';
 import { NAV_LIST } from '@/components/common/Navbar/Navigation';
 import '../styles/Home.module.css';
 import Main from '@/components/main';
+import Landing from '@/components/landing';
 
 const MainPage = () => {
   const setNavAtom = useSetRecoilState(userNavAtom);
@@ -11,10 +13,24 @@ const MainPage = () => {
     activeNavType: NAV_LIST.HOME,
   };
 
-  return (
-    <MainLayout>
-      <Main />
-    </MainLayout>
-  );
+  //TODO: 추후 전역 상태로 관리
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  if (isLogin) {
+    return (
+      <MainLayout>
+        <Main />
+      </MainLayout>
+    );
+  } else {
+    return (
+      <>
+        <Landing />
+        <MainLayout>
+          <Main />
+        </MainLayout>
+      </>
+    );
+  }
 };
 export default MainPage;
