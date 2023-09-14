5528891453d394738632fa2dd180144ff016cc12
Sanghyeon Sim <5338095@gmail.com>
2023-09-15 00:33:30 +0900

    feat: mypage 기능에 닉네임 변경 기능 추가

--- src/components/myPage/EditProfile/EditProfile.styles.tsx
+++ src/components/myPage/EditProfile/EditProfile.styles.tsx
@@ -55,17 +55,64 @@ export const ProfileImageContainer = styled.div`
 
 export const EditProfileButtonContainer = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   margin-top: 20px;
   border-bottom: 1px solid ${COLORS.grayscale.Gray3};
+  width: 70%;
 `;
 
-export const EditProfileTextfield = styled.input`
+export const Width24Container = styled.div`
+  width: 24;
+`;
+
+export const ModalErrorMessage = styled.p`
+  ${TEXT_STYLES.CapR12};
+  color: ${COLORS.SSU.error};
+`;
+
+export const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+interface TextFieldProps {
+  isFailed: boolean;
+}
+
+export const ModalTextfieldContainer = styled.div<TextFieldProps>`
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid ${COLORS.grayscale.Gray3};
+  margin-bottom: 20px;
+
+  align-items: center;
+`;
+
+export const ModalTextfield = styled.input`
   height: 40px;
   border: none;
   text-align: center;
-  background-color: ${COLORS.grayscale.Gray5};
   ${TEXT_STYLES.BodyM16};
-  color: #000000;
+  color: ${COLORS.grayscale.Black};
+  background-color: transparent;
+`;
+
+export const ModalTextLengthCounter = styled.p`
+  ${TEXT_STYLES.CapR14};
+  color: ${COLORS.grayscale.Gray2};
+`;
+
+export const ModalButton = styled.div`
+  width: 100%;
+  height: 40px;
+  border-radius: 10px;
+  background-color: ${COLORS.SSU.primary};
+  color: ${COLORS.grayscale.white};
+  ${TEXT_STYLES.HeadM18};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `;
---
+++ src/pages/my/editprofile/index.page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/common/Header/Header';
+import EditProfile from '@/components/myPage/EditProfile';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <EditProfile />
+    </>
+  );
+}
