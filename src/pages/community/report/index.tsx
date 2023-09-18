2640f4e31b04976ccf57cadee7af34fa633ca669
ChoiSangwon <fdsa200@naver.com>
2023-09-18 22:30:07 +0900

    refactor : 파일 위치 변경

--- src/pages/community/report/index.tsx
+++ src/pages/community/report/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
-import Report from '../components/community/index';
+import Report from '../../../components/community/index';
 import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
