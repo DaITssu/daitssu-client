67d979a84a36a60d7c9117c044edd9291bc19118
jwo0o0 <jwo0o0358@gmail.com>
2023-09-26 00:43:15 +0900

    chore: 안내문구 띄어쓰기 수정

--- src/components/login/NeedLoginModal/NeedLoginModal.tsx
+++ src/components/login/NeedLoginModal/NeedLoginModal.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/common/Button';
 export default function NeedLoginModal() {
   return (
     <styles.ModalContainer>
-      <styles.ModalMessage>로그인 후 이용가능합니다.</styles.ModalMessage>
+      <styles.ModalMessage>로그인 후 이용 가능합니다.</styles.ModalMessage>
       <Button label="로그인" width={326} height={51} />
     </styles.ModalContainer>
   );
