3289f771a5c60c574ef8a95d7d0c4521ac507828
jwo0o0 <jwo0o0358@gmail.com>
2023-09-26 01:16:33 +0900

    feat: 로그인 버튼에 라우터 추가

--- src/components/login/NeedLoginModal/NeedLoginModal.tsx
+++ src/components/login/NeedLoginModal/NeedLoginModal.tsx
@@ -1,11 +1,24 @@
+import { useRouter } from 'next/router';
+
 import * as styles from './NeedLoginModal.styles';
 import Button from '@/components/common/Button';
 
 export default function NeedLoginModal() {
+  const router = useRouter();
+
+  const handleClickLoginButton = () => {
+    router.push('/login');
+  };
+
   return (
     <styles.ModalContainer>
       <styles.ModalMessage>로그인 후 이용 가능합니다.</styles.ModalMessage>
-      <Button label="로그인" width={326} height={51} />
+      <Button
+        label="로그인"
+        width={326}
+        height={51}
+        onClick={handleClickLoginButton}
+      />
     </styles.ModalContainer>
   );
 }
