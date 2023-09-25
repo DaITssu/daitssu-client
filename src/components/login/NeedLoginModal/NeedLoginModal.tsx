42550439f7248c2d7e9bd70199499ea37a7277e1
jwo0o0 <jwo0o0358@gmail.com>
2023-09-25 23:22:39 +0900

    feat:로그인 후 이용 가능 모달 UI 컴포넌트 구현

---
+++ src/components/login/NeedLoginModal/NeedLoginModal.tsx
@@ -0,0 +1,11 @@
+import * as styles from './NeedLoginModal.styles';
+import Button from '@/components/common/Button';
+
+export default function NeedLoginModal() {
+  return (
+    <styles.ModalContainer>
+      <styles.ModalMessage>로그인 후 이용가능합니다.</styles.ModalMessage>
+      <Button label="로그인" width={326} height={51} />
+    </styles.ModalContainer>
+  );
+}
