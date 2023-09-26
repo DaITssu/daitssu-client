288c686a793bd2ab7293d66e5a38a388a17de8a7
SuJin <kimsujin001127@gmail.com>
2023-09-26 20:24:08 +0900

    fix: alt 변경

--- src/components/Home/Inform/NewNews.tsx
+++ src/components/Home/Inform/NewNews.tsx
@@ -8,21 +8,21 @@ export const NewNews = () => {
   return (
     <>
       <InformComponent
-        icon={<Image src={Lecture} alt="check" width={24} height={24} />}
+        icon={<Image src={Lecture} alt="강의" width={24} height={24} />}
         title="오늘 마감인 강의가 있어요!"
         type="none"
         time={3}
         detail='데이터통신 "12주차 강의"'
       />
       <InformComponent
-        icon={<Image src={Post} alt="check" width={24} height={24} />}
+        icon={<Image src={Post} alt="게시물" width={24} height={24} />}
         title="인기 게시판"
         type="none"
         time={3}
         detail="이번주 가장 인기있는 글을 확인해보세요!"
       />
       <InformComponent
-        icon={<Image src={Assignment} alt="check" width={24} height={24} />}
+        icon={<Image src={Assignment} alt="과제" width={24} height={24} />}
         title="오늘 마감인 과제가 있어요!"
         type="none"
         time={3}
