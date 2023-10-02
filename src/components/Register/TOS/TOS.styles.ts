34351ebd4968e309551369be9b554012f714803c
ChoiSangwon <fdsa200@naver.com>
2023-10-02 12:30:49 +0900

    refactor : 100vw -> 100%

--- src/components/Register/TOS/TOS.styles.ts
+++ src/components/Register/TOS/TOS.styles.ts
@@ -43,8 +43,8 @@ export const ModalBackground = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: center;
