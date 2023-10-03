2fbfdf0d3a87e7bbc59fbf28e16530f2525a6e7a
SuJin <kimsujin001127@gmail.com>
2023-10-03 19:19:19 +0900

    chore: 사용안하는 코드 삭제

--- src/components/notice/Layout.tsx
+++ src/components/notice/Layout.tsx
@@ -1,11 +1,9 @@
-import TopBar from './TopBar';
 import TopTab from './TopTab';
 import Anouncement from './anouncement';
 
 const Layout = () => {
   return (
     <div>
-      <TopBar />
       <Anouncement />
       <TopTab />
     </div>
--- src/components/notice/TopTab.tsx
+++ src/components/notice/TopTab.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import NoticeList from './NoticeList';
-import { jsx, css } from '@emotion/react';
 import * as styles from '../../styles/constants/TopTab.style';
 
 const TopTab = () => {
