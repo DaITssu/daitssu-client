36357ee9e2046338638a4ddaf7a467849e121265
jwo0o0 Kim <jwo0o0358@gmail.com>
2023-10-07 16:59:25 +0900

    [#125] axios 예제 코드 추가 (#126)
    * feat: axios 설치
    
    * feat: apis 폴더 path 추가
    
    * feat: axios 객체 생성
    
    * feat: axios request 함수 추가
    
    * feat: api request 예제코드 추가
    
    * fix: 주석 추가
    
    * fix: package-lock.json 삭제
    
    * feat: yarn add axios
    
    * fix: axios 패키지 버전 명시

---
+++ src/apis/axiosInstance.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+//TODO: baseURL 환경변수로 분리
+export const axiosInstance = axios.create({
+  baseURL: 'https://api.daitssu.com',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: '',
+  },
+  //withCredentials: true, //쿠키 사용시 CORS 허용을 위해 true로
+});
