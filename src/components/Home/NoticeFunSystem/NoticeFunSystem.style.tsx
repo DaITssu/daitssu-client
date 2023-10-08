0a7b514779305b7a9898b3d9d4a896374e9bac6d
halfmoon-mind <5338095@gmail.com>
2023-10-08 17:35:41 +0900

    refactor: 기존 notice / funsystem 컴포넌트 수정

--- src/components/Home/NoticeFunSystem/NoticeFunSystem.style.tsx
+++ src/components/Home/NoticeFunSystem/NoticeFunSystem.style.tsx
@@ -1,71 +1,76 @@
 import styled from '@emotion/styled';
 import { TEXT_STYLES } from '@/styles/constants/textStyles';
-export const SummaryBox = styled.div`
-  width : 100%;
-  heigth : 264px;
-  padding : 16px 24px;
-  background: #FFFFFF;
+import { COLORS } from '@/styles/constants/colors';
+
+interface bgColorProps {
+  color?: string;
+}
+
+export const SummaryBox = styled.div<bgColorProps>`
+  width: 100%;
+  padding: 24px 0px;
+  background: ${(props) => props.color ?? COLORS.grayscale.white};
 `;
+
 export const TopBox = styled.div`
-  display : flex; 
+  display: flex;
   justify-content: space-between;
 `;
 
 export const Title = styled.div`
   ${TEXT_STYLES.HeadM20};
-  left : 0px;
- 
+  left: 0px;
 `;
 export const MoreButton = styled.div`
   ${TEXT_STYLES.CapR14};
-  color : #6E6E6E;
-  margin-top : 4px;
-  display : flex;
-  align-items : center;
+  color: ${COLORS.grayscale.Gray1};
+  margin-top: 4px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 `;
 
 export const TextBox = styled.div`
   box-sizing: border-box;
   position: relative;
   width: 100%;
-  height : 170px;
-  margin-top : 16px;
-  background: #FFFFFF;
-  border: 1px solid #D9D9D9;
+  height: 170px;
+  margin-top: 16px;
+  background: #ffffff;
+  border: 1px solid ${COLORS.grayscale.Gray3};
   box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.05);
   border-radius: 15px;
 `;
 
-export const Line = styled.div<{top : string}>`
-  width : 100%;
-  height : 1px;
-  top : ${(props)=> props.top};
-  position : absolute;
-  background-color : #D9D9D9;
+export const Line = styled.div<{ top: string }>`
+  width: 100%;
+  height: 1px;
+  top: ${(props) => props.top};
+  position: absolute;
+  background-color: #d9d9d9;
 `;
 
-export const TextLine = styled.div<{top : string}>`
-  display : flex; 
-  width : 100%;
-  position : absolute;
-  // margin : 0px 12px;
-  top : ${(props)=> props.top};
-  box-sizing : border-box;
+export const TextLine = styled.div<{ top: string }>`
+  display: flex;
+  width: 100%;
+  position: absolute;
+  top: ${(props) => props.top};
+  box-sizing: border-box;
 `;
 
 export const TextSummary = styled.div`
   ${TEXT_STYLES.BodyM16};
-  white-space : nowrap;
-  overflow : hidden;
-  display : auto;
-  text-overflow : ellipsis;
-  padding-right : 50px;
+  white-space: nowrap;
+  overflow: hidden;
+  display: auto;
+  text-overflow: ellipsis;
+  padding-right: 50px;
   padding-left: 12px;
 `;
 
 export const Date = styled.div`
   ${TEXT_STYLES.CapR14};
-  color : #6E6E6E;
-  position : absolute;
-  right : 12px;
+  color: ${COLORS.grayscale.Gray1};
+  position: absolute;
+  right: 12px;
 `;
--- src/components/Home/NoticeFunSystem/NoticeFunSystem.tsx
+++ src/components/Home/NoticeFunSystem/NoticeFunSystem.tsx
@@ -2,51 +2,50 @@ import { useState } from 'react';
 import * as styles from './NoticeFunSystem.style';
 import RightArrow from '@icons/icon/Arrow/RightSmallArrow.svg';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-interface SummaryText{
-  textContent : string;
-  date : string;
+export interface SummaryText {
+  textContent: string;
+  date: string;
 }
 
-export interface NoticeFunSystemProps{
-  title : string;
-  summaries : SummaryText[];
+export interface NoticeFunSystemProps {
+  title: string;
+  summaries: SummaryText[];
+  color?: string;
 }
 
+const NoticeFunSystem = (props: NoticeFunSystemProps) => {
+  const router = useRouter();
 
-const NoticeFunSystem= (props:NoticeFunSystemProps) => {
+  function onTapMore() {
+    //TODO 더보기 라우팅 연결
+    router.push('');
+  }
 
   return (
-      <styles.SummaryBox>
-        <styles.TopBox>
-          <styles.Title>
-            {props.title}
-          </styles.Title>
-          <styles.MoreButton>
-            더보기
-            <Image src ={RightArrow} alt = "right arrow"/>
-          </styles.MoreButton>
-        </styles.TopBox >
-        
-        <styles.TextBox>
-          <styles.Line top="55px"/>
-          <styles.Line top="112px"/>
-          {props.summaries.map((summary, index) => (
-            <div key={index}>
-              <styles.TextLine top={`${16 + index * 57}px`}>
-                <styles.TextSummary>
-                  {summary.textContent}
-                </styles.TextSummary>
-                <styles.Date>
-                  {summary.date}
-                </styles.Date>
-              </styles.TextLine>
-              
-            </div>
-          ))}
-        </styles.TextBox>
-      </styles.SummaryBox>
-      
+    <styles.SummaryBox color={props.color}>
+      <styles.TopBox>
+        <styles.Title>{props.title}</styles.Title>
+        <styles.MoreButton onClick={onTapMore}>
+          더보기
+          <Image src={RightArrow} alt="right arrow" />
+        </styles.MoreButton>
+      </styles.TopBox>
+
+      <styles.TextBox>
+        <styles.Line top="55px" />
+        <styles.Line top="112px" />
+        {props.summaries.map((summary, index) => (
+          <div key={index}>
+            <styles.TextLine top={`${16 + index * 57}px`}>
+              <styles.TextSummary>{summary.textContent}</styles.TextSummary>
+              <styles.Date>{summary.date}</styles.Date>
+            </styles.TextLine>
+          </div>
+        ))}
+      </styles.TextBox>
+    </styles.SummaryBox>
   );
 };
 
