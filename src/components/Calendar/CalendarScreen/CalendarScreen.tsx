c278f77a3aa014903c0438d259e84151da9deb4c
halfmoon-mind <5338095@gmail.com>
2023-11-13 20:44:15 +0900

    feat: 캘린더 스크린 API 연결

--- src/components/Calendar/CalendarScreen/CalendarScreen.tsx
+++ src/components/Calendar/CalendarScreen/CalendarScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SubjectList from '../SubjectList';
 import { SubjectDTOExample } from '@/types/Subject';
 import Calendar from '../Calendar/Calendar';
@@ -10,14 +10,16 @@ import EditEventModal from '../EditEventModal';
 import CalendarIcon from '@icons/icon/Icon24/CalenderUpdate.svg';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { getCalendarAPI } from '@/apis/courseAPIS';
 
 const CalendarScreen = () => {
   const router = useRouter();
   const { open, PopUp, close } = useModal();
+  let today = new Date();
 
-  const [year, setYear] = useState<number>(2023);
-  const [month, setMonth] = useState<number>(8);
-  const [selectDay, setSelectDay] = useState<number>(23);
+  const [year, setYear] = useState<number>(today.getFullYear());
+  const [month, setMonth] = useState<number>(today.getMonth() + 1);
+  const [selectDay, setSelectDay] = useState<number>(today.getDate());
   const [dayTasks, setDayTasks] = useState<{ [key: number]: Array<string> }>(
     {},
   );
@@ -29,12 +31,31 @@ const CalendarScreen = () => {
   function onMonthChange(year: number, month: number) {
     setYear(year);
     setMonth(month);
+    let current = new Date(year, month - 1, 1);
+    const getCalendarResponse = getCalendarAPI(
+      `${current.getFullYear()}-${current.getMonth() + 1}`,
+    );
+    getCalendarResponse.then((res) => {
+      //TODO : 서버에서 받아온 데이터를 dayTasks에 저장
+      console.log(res);
+    });
   }
 
   function onEditAlarmClick() {
     //TODO 알림 화면에서 보여줄 데이터 전달
     router.push('/calendar/alarm');
   }
+
+  useEffect(() => {
+    let current = new Date();
+    // 이번달 데이터 저장
+    const getCalendarResponse = getCalendarAPI(
+      `${current.getFullYear()}-${current.getMonth() + 1}`,
+    );
+    getCalendarResponse.then((res) => {
+      console.log(res);
+    });
+  }, []);
 
   // now
   const now = new Date();
