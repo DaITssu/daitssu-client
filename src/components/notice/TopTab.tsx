67977c8cbfa627a14df505d41dcb792218941337
JooHui-void <elis100@naver.com>
2023-11-17 03:19:27 +0900

    feat : api 적용

--- src/components/notice/TopTab.tsx
+++ src/components/notice/TopTab.tsx
@@ -1,27 +1,73 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NoticeList from './NoticeList';
-import { jsx, css } from '@emotion/react';
 import * as styles from './TopTab.styles';
 import SearchBar from './anouncement/SearchBar'
 import ButtonGroup from './anouncement/SelectButton';
+import { getNoticeAPI, getNoticeAPIwithCategory } from '@/apis/noticeAPIS';
 
 
 const TopTab = () => {
-  const data = [
+  
+
+  const [data,setData] = useState([
     {
       id: 0,
-      title: '공지사항',
-   
+      title: '공지사항',  
       contents: <NoticeList items = {[]}/>,
     },
     {
       id: 1,
       title: '펀시스템',
-      // TODO: noticelist 추가 필요 (디자인 수정 필요)
       contents: <NoticeList items = {[]}/>,
     },
-  ];
+  ]);
   const [index, setIndex] = useState(0);
+  const [category, setCategory] = useState("ALL");
+
+  const fetchData = async (searchText?: String, nowCategory?:String) => {
+    try{
+      if(nowCategory){
+        const noticeData = await getNoticeAPIwithCategory(searchText, nowCategory);
+        if(noticeData){
+          setData((prevData)=>[
+            {...prevData[0], contents: <NoticeList items={noticeData.data.content} />},
+            { ...prevData[1], contents: <NoticeList items={[]} /> },
+          ]);
+          
+        }else{
+          console.error('공지사항 데이터를 불러오는 중 오류 발생:', noticeData.message);
+        }
+      }else{ 
+        const noticeData = await getNoticeAPIwithCategory(searchText,category);
+        if(noticeData){
+          setData((prevData)=>[
+            {...prevData[0], contents: <NoticeList items={noticeData.data.content} />},
+            { ...prevData[1], contents: <NoticeList items={[]} /> },
+          ]);
+          
+        }else{
+          console.error('공지사항 데이터를 불러오는 중 오류 발생:', noticeData.message);
+        }
+      }
+      
+    }catch(error){
+      console.error('공지사항 데이터를 불러오는 중 오류 발생:', error);
+    }
+  };
+
+
+  useEffect(()=>{
+    fetchData();
+  }, []);
+
+  const handleSearch = (searchText : String) =>{
+    fetchData(searchText);
+  }
+  const handleCategoryButton = (category :string) =>{
+    setCategory(category);
+    fetchData("",category);
+  }
+
 
   const selectMenuHandler = (n: number) => {
     setIndex(n);
@@ -40,8 +86,8 @@ const TopTab = () => {
           </styles.TabFontBox>
         ))}
       </styles.Rectangle242>
-      <SearchBar/>
-      <ButtonGroup/>
+      <SearchBar onSearch={handleSearch}/>
+      <ButtonGroup category={handleCategoryButton}/>
       {data[index].contents}
      
     </styles.TabContentBox>
