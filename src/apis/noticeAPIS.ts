b510b32716cacf5906762fab1b3759cb143b185d
JooHui-void <69834729+JooHui-void@users.noreply.github.com>
2023-11-24 01:05:09 +0900

    [#141] 공지사항/펀시스템 (4-1,4-2) API 연결  (#142)
    * fix : 검색창, 버튼창 width max로 변경
    
    * fix : 파일 구조, 컴포넌트 구조 변경
    
    * feat : notice API 추가
    
    * fix : notice 변경 요소 props로 꺼내기
    
    * feat : 받아오는 데이터 형식 변경
    
    * feat : enum 매핑 추가
    
    * feat : api 적용
    
    * feat : 펀시스템 카테고리 값 추가
    
    * feat : 펀시스템 API 추가
    
    * feat : 펀시스템 API 전환 추가
    
    * feat : 페이지네이션 무한스크롤 추가

---
+++ src/apis/noticeAPIS.ts
@@ -0,0 +1,78 @@
+import { axiosInstance } from "@/apis/axiosInstance";
+import axios from "axios";
+
+//Todo : 페이지네이션 
+export const getNoticeAPI = async (searchKeyword:String="") => {
+  try {
+    const response = await axiosInstance.get(`/notice`,{
+      params : {
+        searchKeyword : String(searchKeyword),
+      }
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    }
+    return null;
+  }
+};
+
+
+export const getNoticeAPIwithCategory = async (searchKeyword:String ="",category :String, page:number =0) => {
+  try {
+    if(category === "ALL"){
+      const response = await axiosInstance.get(`/notice`,{
+        params : {
+          searchKeyword : String(searchKeyword),
+          page : page,
+        }
+      });
+      return response.data;
+    }else{
+      const response = await axiosInstance.get(`/notice/${category}`,{
+        params : {
+          searchKeyword : String(searchKeyword),
+          page : page,
+        }
+      });
+      return response.data;
+    }
+    
+    
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    }
+    return null;
+  }
+};
+
+export const getFunSystemAPIwithCategory = async (searchKeyword:String ="",category :String, page:number =0) => {
+  try {
+    if(category === "ALL"){
+      const response = await axiosInstance.get(`/funsystem`,{
+        params : {
+          searchKeyword : String(searchKeyword),
+          page : page,
+        }
+      });
+      return response.data;
+    }else{
+      const response = await axiosInstance.get(`/funsystem/${category}`,{
+        params : {
+          searchKeyword : String(searchKeyword),
+          page : page,
+        }
+      });
+      return response.data;
+    }
+    
+    
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    }
+    return null;
+  }
+};
--- src/components/notice/TopTab.tsx
+++ src/components/notice/TopTab.tsx
@@ -1,49 +1,118 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NoticeList from './NoticeList';
-import { jsx, css } from '@emotion/react';
 import * as styles from './TopTab.styles';
-import SearchBar from './anouncement/SearchBar'
+import SearchBar from './anouncement/SearchBar';
 import ButtonGroup from './anouncement/SelectButton';
+import { getFunSystemAPIwithCategory, getNoticeAPIwithCategory } from '@/apis/noticeAPIS';
+import { NoticeItemProps } from './NoticeItem';
 
 
 const TopTab = () => {
-  const data = [
-    {
-      id: 0,
-      title: '공지사항',
-   
-      contents: <NoticeList items = {[]}/>,
-    },
-    {
-      id: 1,
-      title: '펀시스템',
-      // TODO: noticelist 추가 필요 (디자인 수정 필요)
-      contents: <NoticeList items = {[]}/>,
-    },
-  ];
+  
+  const [noticeData, setNoticeData] = useState<NoticeItemProps[]>([]);
+  const [funSystemData, setFunSystemData] = useState<NoticeItemProps[]>([]);
   const [index, setIndex] = useState(0);
+  const [category, setCategory] = useState("ALL");
+  
+  const [fetching, setFetching] = useState(false); //데이터 받아오는 중인지 표시
+  const [page, setPage] = useState(0);
+  const [search, setSearch] = useState("");
+
+  const fetchNoticeData = async () => {
+
+    try{
+      if(page ===0){
+        setNoticeData([]);
+      }
+      const noticeData = await getNoticeAPIwithCategory(search,category,page);
+      if(noticeData){
+        setNoticeData((prev)=>[...prev,...noticeData.data.content]);
+      }else{
+        console.error('공지사항 데이터를 불러오는 중 오류 발생:', noticeData.message);
+      } 
+      
+    }catch(error){
+      console.error('공지사항 데이터를 불러오는 중 오류 발생:', error);
+    }
+  };
+
+  const fetchFunSystemData = async () => {
+
+    try{
+      if(page ===0){
+        setFunSystemData([]);
+      }
+      const funSystemData = await getFunSystemAPIwithCategory(search,category,page);
+      if(funSystemData){
+        setFunSystemData((prev)=>[...prev,...funSystemData.data.content]);
+      }else{
+        console.error('펀시스템 데이터를 불러오는 중 오류 발생:', funSystemData.message);
+      }
+    
+    }catch(error){
+      console.error('펀시스템 데이터를 불러오는 중 오류 발생:', error);
+    }
+  };
+
+  useEffect(()=>{
+    window.addEventListener("scroll",handleScroll);
+
+    fetchNoticeData();
+    fetchFunSystemData();
+
+    return() =>{
+      window.removeEventListener("scroll",handleScroll);
+    }
+  }, [category, index, search]);
+
+  const handleSearch = (searchText : string) =>{
+    setPage(0);
+    setSearch(searchText);
+  }
+  const handleCategoryButton = (category :string) =>{
+    setPage(0);
+    setCategory(category);
+  }
+
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight >= scrollHeight - 200 && !fetching) {
+      index ? fetchFunSystemData():fetchNoticeData(); //TODO : 검색까지 반영되게 
+    }
+   };
 
   const selectMenuHandler = (n: number) => {
     setIndex(n);
+    setPage(0);
+    setCategory("ALL");
+    setSearch("");
   };
 
   return (
     <styles.TabContentBox>
       <styles.Rectangle242>
-        {data.map((element, idx) => (
+        {["공지사항", "펀시스템"].map((title, idx) => (
           <styles.TabFontBox
-            key={element.id}
+            key={idx}
             isSelected={idx === index}
             onClick={() => selectMenuHandler(idx)}
           >
-            {element.title}
+            {title}
           </styles.TabFontBox>
         ))}
       </styles.Rectangle242>
-      <SearchBar/>
-      <ButtonGroup/>
-      {data[index].contents}
-     
+      <SearchBar onSearch={handleSearch}/>
+      <ButtonGroup index = {index} category={handleCategoryButton}/>
+
+      {index === 0 ? (
+        <NoticeList items={noticeData}/>
+      ) : (
+        <NoticeList items={funSystemData} />
+      )}
+      
+
     </styles.TabContentBox>
   );
 };
