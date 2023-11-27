2b52744f47143fa1b37fbc832d2b376d077cb1cb
JooHui-void <elis100@naver.com>
2023-11-27 21:46:08 +0900

    feat : 헤더 돋보기에 서칭기능 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,16 +2,29 @@ import * as styles from './Header.style';
 import SearchIcon from '@icons/icon/header/search.svg';
 import AlarmIcon from '@icons/icon/header/alarm.svg';
 import Image from 'next/image';
+import { useState } from 'react';
+import LineSearchBar from '@/components/Home/NoticeFunSystemTab/LineSearchBar/LineSearchBar';
+import NoticeFunSystemTab from '@/components/Home/NoticeFunSystemTab/NoticeFunSystemTab';
 const Header = () => {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const handleSearchIconClick=()=>{
+    setIsSearchVisible((prev)=> !prev);
+  }
+
   return (
     <>
       <styles.Conatiner>
         <styles.LogoText>DAITSSU</styles.LogoText>
         <styles.IconContainer>
-          <Image src={SearchIcon} height={24} width={24} alt="search" />
+          <Image  src={SearchIcon} 
+                  height={24} 
+                  width={24} 
+                  alt="search" 
+                  onClick={handleSearchIconClick} />
           <Image src={AlarmIcon} height={24} width={24} alt="alarm" />
         </styles.IconContainer>
       </styles.Conatiner>
+      {isSearchVisible && <NoticeFunSystemTab/>}
     </>
   );
 };
