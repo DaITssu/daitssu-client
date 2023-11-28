f1bde6c7bb5481ee1e27e7e52f70d97650365ac8
halfmoon-mind <5338095@gmail.com>
2023-11-28 20:43:54 +0900

    feat: 메인화면 컴포넌트에 추가

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,13 +1,41 @@
-import styled from '@emotion/styled';
-
-const StyledDiv = styled.div`
-  height: 2000px;
-`;
+import NoticeFunSystem, {
+  SummaryText,
+} from '../Home/NoticeFunSystem/NoticeFunSystem';
+import PopularPosts, {
+  PostDetailProps,
+} from '../Home/PopularPosts/PopularPosts';
+import { useEffect, useState } from 'react';
+import { getFunsystem, getNotice, getPopularCommunity } from '@/apis/mainAPIs';
 
 const Main = () => {
+  // load datas
+  const [posts, setPosts] = useState<PostDetailProps[]>([]);
+  const [noticeList, setNoticeList] = useState<SummaryText[]>([]);
+  const [funSystemList, setFunSystemList] = useState<SummaryText[]>([]);
+
+  useEffect(() => {
+    const popularArticleResponse = getPopularCommunity();
+    const noticeResponse = getNotice();
+    const funSystemResponse = getFunsystem();
+
+    popularArticleResponse.then((data) => {
+      setPosts(data.data);
+    });
+
+    noticeResponse.then((data) => {
+      setNoticeList(data.data);
+    });
+
+    funSystemResponse.then((data) => {
+      setFunSystemList(data.data);
+    });
+  }, []);
+
   return (
     <>
-      <StyledDiv>home화면입니다</StyledDiv>
+      <PopularPosts posts={posts} />
+      <NoticeFunSystem title="공지사항" summaries={noticeList} />
+      <NoticeFunSystem title="펀시스템" summaries={funSystemList} />
     </>
   );
 };
