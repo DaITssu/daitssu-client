7e3ac2acd12a21c6711da31ea11ef7416e13faea
SuJin <kimsujin001127@gmail.com>
2023-11-28 12:58:31 +0900

    feat: 공지사항 API 연결

--- src/components/notice/info/NoticeInfo.tsx
+++ src/components/notice/info/NoticeInfo.tsx
@@ -10,9 +10,11 @@ import Download from '@icons/icon/Icon24/Download.svg';
 import Calendar from '@icons/icon/Nav/calendar_off.svg';
 import Url from '@icons/icon/Icon18/Url.svg';
 import Kakao from '@icons/icon/Icon18/Kakao.svg';
-import { getNoticeInfoAPI } from '@/apis/noticeAPIs';
+import { getNoticeInfoAPI } from '@/apis/noticeAPIS';
 import { NoticeInfoProps } from '@/types/NoticeFunsystem';
 import UtilityHeader from '@/components/common/Header/UtilityHeader';
+import { useRouter } from 'next/router';
+import { getKor } from '../CategoryMapping';
 
 const NoticeInfo = () => {
   const [noticeData, setNoticeData] = useState<NoticeInfoProps>();
@@ -20,11 +22,19 @@ const NoticeInfo = () => {
   const handleShareClick = () => {
     setShare(!share);
   };
+
+  // 현재 주소 값
+  const router = useRouter();
+  const path = router.asPath;
+  const numericPart = path.match(/\d+/);
+  const pathId = numericPart ? numericPart[0] : 1;
+
+  // 공지사항 API 연결
   useEffect(() => {
-    // TODO: 들어오는 아이디 값으로 변경 필요
-    const getNoticeInfo = getNoticeInfoAPI(3367);
+    const getNoticeInfo = getNoticeInfoAPI(Number(pathId));
     getNoticeInfo.then((res) => {
       setNoticeData(res.data);
+      console.log(res.data);
     });
   }, []);
 
@@ -32,7 +42,7 @@ const NoticeInfo = () => {
     <styles.Container>
       <UtilityHeader child="공지사항" />
       <styles.InfoBox>
-        <styles.TypeBox>학사</styles.TypeBox>
+        <styles.TypeBox>{getKor(noticeData.category)}</styles.TypeBox>
         <styles.TitleBox>{noticeData?.title}</styles.TitleBox>
         <styles.MiddleBox>
           <styles.DateBox>
@@ -109,39 +119,40 @@ const NoticeInfo = () => {
           </styles.ShareBox>
         </styles.MiddleBox>
         <hr />
-        <styles.ContentBox>
-          2023-1학기 다전공 신청기간 안내드립니다. 2023-1학기 다전공 신청기간
-          안내드립니다. 2023-1학기 다전공 신청기간 안내드립니다.
-        </styles.ContentBox>
-        <hr />
-        <styles.FileBox>
-          <styles.FileTopBox>
-            <styles.FileIconBox>
-              <Image
-                src={AttchedFile}
-                alt="attchment"
-                width={20}
-                height={20}
-                priority
-              />
-            </styles.FileIconBox>
-            <styles.FileTitleBox>첨부파일</styles.FileTitleBox>
-          </styles.FileTopBox>
-          <styles.FileAttachBox>
-            <styles.FileNameBox>
-              2023-1학기-전공별-다전공-선발-방법.xlsx
-            </styles.FileNameBox>
-            <styles.FileDownBox>
-              <Image
-                src={Download}
-                alt="download button"
-                width={20}
-                height={20}
-                priority
-              />
-            </styles.FileDownBox>
-          </styles.FileAttachBox>
-        </styles.FileBox>
+        <styles.ContentBox>{noticeData?.content}</styles.ContentBox>
+        {noticeData?.fileUrl.length !== 0 && (
+          <>
+            <hr />
+            <styles.FileBox>
+              <styles.FileTopBox>
+                <styles.FileIconBox>
+                  <Image
+                    src={AttchedFile}
+                    alt="attchment"
+                    width={20}
+                    height={20}
+                    priority
+                  />
+                </styles.FileIconBox>
+                <styles.FileTitleBox>첨부파일</styles.FileTitleBox>
+              </styles.FileTopBox>
+              <styles.FileAttachBox>
+                <styles.FileNameBox>
+                  2023-1학기-전공별-다전공-선발-방법.xlsx
+                </styles.FileNameBox>
+                <styles.FileDownBox>
+                  <Image
+                    src={Download}
+                    alt="download button"
+                    width={20}
+                    height={20}
+                    priority
+                  />
+                </styles.FileDownBox>
+              </styles.FileAttachBox>
+            </styles.FileBox>
+          </>
+        )}
       </styles.InfoBox>
       <styles.BottomBox>
         <styles.CommentTitleBox>댓글</styles.CommentTitleBox>
