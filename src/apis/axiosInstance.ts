3c929c893b8c1a446e080e212ae0ce866ca44ba2
halfmoon-mind <5338095@gmail.com>
2023-11-30 09:28:57 +0900

    feat: 임시 토큰 부여

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({
   baseURL: 'https://api.daitssu.com',
   headers: {
     'Content-Type': 'application/json',
-    Authorization: '',
+    Authorization: 'Bearer test',
   },
   //withCredentials: true, //쿠키 사용시 CORS 허용을 위해 true로
 });
