8551debe79d38d3bc2a38172ce159c3adf37d75f
SuJin <kimsujin001127@gmail.com>
2023-12-04 20:54:58 +0900

    refactor: eslint 정렬 수정

--- src/components/notice/NoticeItem.tsx
+++ src/components/notice/NoticeItem.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import view from '@icons/icon/Icon18/SmallHits.svg';
 import { getKor } from './CategoryMapping';
 
-
 export interface NoticeItemProps {
   id: number;
   title: string;
@@ -17,19 +16,20 @@ export interface NoticeItemProps {
 interface Props {
   item: NoticeItemProps;
 }
-const formattedDate = (originalDate:String) =>{
-  const parts = originalDate.split("T")[0].split("-");
+const formattedDate = (originalDate: String) => {
+  const parts = originalDate.split('T')[0].split('-');
   return `${parts[0].slice(2)}/${parts[1]}/${parts[2]}`;
 };
 const NoticeItem: React.FC<Props> = ({ item }: Props) => {
   const notice_onClick = () => {};
+
   return (
     <Link href={`/notice/${item.id}`}>
       <styles.Noticeitem onClick={notice_onClick}>
         <styles.NoticeStatus>{getKor(item.category)}</styles.NoticeStatus>
 
         <styles.NoticeTitleFont>{item.title}</styles.NoticeTitleFont>
-        <styles.ViewIcon src= {view} width={18} height={18} alt={'view_icon'}  />
+        <styles.ViewIcon src={view} width={18} height={18} alt={'view_icon'} />
         <styles.NoticeViews>
           {item.views.toLocaleString('ko-KR')}회
         </styles.NoticeViews>
