ae557e6ece362a5c0f6bd313d7838c3e3c21d447
jwo0o0 <jwo0o0358@gmail.com>
2023-12-24 21:34:33 +0900

    feat: 로그인 API 연결, 응답코드에 따라 분기 처리

--- src/components/login/LoginLayout/LoginLayout.tsx
+++ src/components/login/LoginLayout/LoginLayout.tsx
@@ -1,61 +1,96 @@
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import * as styles from './LoginLayout.styles';
 
 import InputBox from '@/components/common/InputBox/InputBox';
 import AutoLogon from '../AutoLogon/AutoLogon';
 import Button from '@/components/common/Button';
 import FindAuth from '../FindAuth/FindAuth';
+import Modal from '@/components/common/Modal';
+import LoginErrorModal from '../LoginErrorModal/LoginErrorModal';
+import useModal from '@/components/common/Modal/useModal';
+
+import LoginProcess from '../LoginProcess/LoginProcess';
 
 import { signInAPI } from '@/apis/authAPIS';
 
 export default function LoginLayout() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const [ID, setID] = useState<number>();
   const [password, setPassword] = useState<string>('');
   const [isSelectedAutoLogon, setIsSelectedAutoLogon] =
     useState<boolean>(false);
+
+  //모달
+  const { open, PopUp, close } = useModal();
+  const [message, setMessage] = useState('');
+
+  const router = useRouter();
 
   const handleClickAutoLogon = () => {
     setIsSelectedAutoLogon(!isSelectedAutoLogon);
   };
 
   const handleClickLoginBtn = () => {
+    setIsLoading(true);
     const signInResponse = signInAPI(String(ID), password);
-    signInResponse.then((res) => {
-      console.log(res);
-      if (res?.code === 1001) {
-        alert('회원 정보를 찾을 수 없습니다.');
-        setPassword('');
-      } else if (res?.code === 0) {
-        console.log('로그인 성공');
-      }
-    });
+    signInResponse
+      .then((res) => {
+        if (res?.code === 0) {
+          //로그인 성공
+          setIsLoading(false);
+          alert('로그인 성공');
+        } else if (res?.code === 1001) {
+          //회원가입
+          router.push('/register');
+        } else {
+          //로그인 실패
+          setIsLoading(false);
+          setMessage(res?.message || '');
+        }
+      })
+      .then(() => {
+        open();
+      });
   };
 
   return (
-    <styles.LayoutContainer>
-      <styles.Content>
-        <styles.LoginHeader>숭실대 통합 LOGIN</styles.LoginHeader>
-        <InputBox
-          value={ID}
-          setValue={setID}
-          label="ID"
-          type="number"
-          placeholder="학번"
-        />
-        <InputBox
-          value={password}
-          setValue={setPassword}
-          label="Password"
-          type="password"
-          placeholder="비밀번호"
-        />
-        <AutoLogon
-          isSelected={isSelectedAutoLogon}
-          handleClickAutoLogon={handleClickAutoLogon}
-        />
-        <Button label="로그인" height={51} onClick={handleClickLoginBtn} />
-        <FindAuth />
-      </styles.Content>
-    </styles.LayoutContainer>
+    <>
+      {isLoading ? (
+        <LoginProcess />
+      ) : (
+        <styles.LayoutContainer>
+          <styles.Content>
+            <styles.LoginHeader>숭실대 통합 LOGIN</styles.LoginHeader>
+            <InputBox
+              value={ID}
+              setValue={setID}
+              label="ID"
+              type="number"
+              placeholder="학번"
+            />
+            <InputBox
+              value={password}
+              setValue={setPassword}
+              label="Password"
+              type="password"
+              placeholder="비밀번호"
+            />
+            <AutoLogon
+              isSelected={isSelectedAutoLogon}
+              handleClickAutoLogon={handleClickAutoLogon}
+            />
+            <Button label="로그인" height={51} onClick={handleClickLoginBtn} />
+            <FindAuth />
+          </styles.Content>
+          <PopUp>
+            <Modal onClose={close} title={''}>
+              <LoginErrorModal message={message} close={close} />
+            </Modal>
+          </PopUp>
+        </styles.LayoutContainer>
+      )}
+    </>
   );
 }
