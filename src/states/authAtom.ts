378a53d3e1c9aa2363150215b9816b54008881d9
jwo0o0 <jwo0o0358@gmail.com>
2023-12-26 16:23:22 +0900

    feat: 로그인 관련 atom 설정(로그인 여부, accessToken)

---
+++ src/states/authAtom.ts
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+import { persistAtom } from './userNavAtom';
+
+const isBrowser = typeof window !== 'undefined';
+
+export const loginAtom = atom<boolean>({
+  key: 'loginAtom',
+  default: false,
+  effects_UNSTABLE: isBrowser ? [persistAtom] : [],
+});
+
+export const accessTokenAtom = atom<string>({
+  key: 'accessToken',
+  default: '',
+  effects_UNSTABLE: isBrowser ? [persistAtom] : [],
+});
