d539a0e0da968cea1489a5335ac24508ed61a5ba
jwo0o0 <jwo0o0358@gmail.com>
2023-12-26 16:24:09 +0900

    feat: axios 인터셉터로 request 헤더에 accessToken 추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,11 +1,26 @@
 import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { accessTokenAtom } from '@/states/authAtom';
 
 //TODO: baseURL 환경변수로 분리
 export const axiosInstance = axios.create({
   baseURL: 'https://api.daitssu.com',
   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer test',
   },
   //withCredentials: true, //쿠키 사용시 CORS 허용을 위해 true로
 });
+
+//request Header에 accessToken 설정
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const accessToken = useRecoilValue(accessTokenAtom);
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
