06fc5ce3ebcdde32e2cfcf6658c3d3ab55e582ac
김수진 <kimsujin001127@gmail.com>
2024-01-02 19:34:01 +0900

    [#161] 상세정보 반응형으로 수정 (#162)
    * style: 공지사항/펀시스템 상세정보 반응형으로 변경
    
    * style: 댓글 입력 반응형으로 수정

--- src/components/common/Comment/CommentInput/CommentInput.styles.ts
+++ src/components/common/Comment/CommentInput/CommentInput.styles.ts
@@ -4,34 +4,38 @@ import styled from '@emotion/styled';
 
 export const MyCommentBox = styled.div`
   margin: auto;
-  width: 358px;
   height: 45px;
+  width: 90%;
 `;
 
 export const CommentFlexBox = styled.div`
   position: fixed;
   top: 90%;
   display: flex;
-  width: 358px;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  padding: 0 12px;
 `;
 
 export const MyLeftBox = styled.div`
   margin-right: auto;
+  width: 100%;
 `;
 
 export const MyRightBox = styled.div`
-  margin-left: auto;
+  margin-left: 10px;
 `;
 
 export const MyCommentLabel = styled.label`
   input {
     background-color: ${COLORS.grayscale.Gray4};
-    width: 293px;
     height: 45px;
     border-radius: 10px;
     border: none;
     padding-left: 10px;
     outline: none;
+    width: 100%;
   }
   input::placeholder {
     color: ${COLORS.grayscale.Gray1};
