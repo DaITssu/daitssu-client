1bf1d8e11a79bbb2fb830591d4b842dc02cd6f32
JooHui-void <elis100@naver.com>
2024-01-09 20:13:03 +0900

    fix: 스토리북 수정

--- src/components/mypage/ServiceNotice/info/ServiceNoticeInfo.stories.tsx
+++ src/components/mypage/ServiceNotice/info/ServiceNoticeInfo.stories.tsx
@@ -8,18 +8,14 @@ export default {
   title: 'MyPage/ServiceNoticeInfo',
 };
 
-const Template: Story<ServiceNoticeInfoProps> = (args: ServiceNoticeInfoProps) => (
-  <ServiceNoticeInfo {...args} />
-);
+const Template: Story<ServiceNoticeInfoProps> = (
+  args: ServiceNoticeInfoProps,
+) => <ServiceNoticeInfo {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   title: '6/23 (금) 시스템 점검 및 업데이트 안내',
-  date: '2023/05/11',
-  content: '안녕하세요 다잇슈입니다. 더욱 쾌적하고 안정적인 서비스 지원을 위해 아래와 같이 점검이 진행됩니다.',
+  createdAt: '2023/05/11',
+  content:
+    '안녕하세요 다잇슈입니다. 더욱 쾌적하고 안정적인 서비스 지원을 위해 아래와 같이 점검이 진행됩니다.',
 };
-
-
-
-
-
