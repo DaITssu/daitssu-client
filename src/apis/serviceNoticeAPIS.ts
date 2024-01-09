04e48d6f782434e3a0dc12dde3058a2cb20ce764
JooHui-void <elis100@naver.com>
2024-01-09 20:06:53 +0900

    fix: api url 수정

--- src/apis/serviceNoticeAPIS.ts
+++ src/apis/serviceNoticeAPIS.ts
@@ -1,12 +1,9 @@
-import axios from "axios";
-import { axiosInstance } from "./axiosInstance";
+import axios from 'axios';
+import { axiosInstance } from './axiosInstance';
 
 export const getServiceNoticeAPI = async () => {
   try {
-    const response = await axiosInstance.get(`/mypage/service-notice`,{
-      
-    });
-    // console.log(response.data);
+    const response = await axiosInstance.get(`/myPage/service-notice`);
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
