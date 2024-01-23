991244aa48052e0ae24f3b35bc96103f5532ae68
SuJin <kimsujin001127@gmail.com>
2024-01-23 19:15:02 +0900

    feat: 내글목록 헤더 설정
    TODO: 삭제 버튼 함수 구현 필요

--- src/components/myPage/Post/MyPost.tsx
+++ src/components/myPage/Post/MyPost.tsx
@@ -1,3 +1,4 @@
+import UtilityHeader from '@/components/common/Header/UtilityHeader';
 import * as styles from './Mypost.style';
 import MyPostsTab from './Tab/MyPostsTab';
 import MainLayout from '@/pages/layout';
@@ -5,6 +6,13 @@ import MainLayout from '@/pages/layout';
 const MyPost = () => {
   return (
     <styles.Container>
+      <UtilityHeader
+        child="내 글 목록"
+        isDeleteBtn={true}
+        // TODO: 삭제함수버튼 구현
+        // onClickDeleteBtn={}
+      />
+
       <MainLayout>
         <MyPostsTab />
       </MainLayout>
