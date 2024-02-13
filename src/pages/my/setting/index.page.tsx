1a5c90ee93bf67cb83fbec17485c4ea7e1ba39a9
jwo0o0 <jwo0o0358@gmail.com>
2024-02-13 13:59:35 +0900

    fix: 마이페이지 관련 페이지들에 Header 중복으로 있던 것 삭제

--- src/pages/my/setting/index.page.tsx
+++ src/pages/my/setting/index.page.tsx
@@ -1,11 +1,8 @@
-import Header from '@/components/common/Header/Header';
-import NavigationBar from '@/components/common/Navbar/NavigationBar';
 import Setting from '@/components/myPage/setting/setting';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Setting />
       {/* <NavigationBar focusType={'MY_PAGE'} /> */}
     </>
