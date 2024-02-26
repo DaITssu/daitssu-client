30d52bca9203a20b711e97be80c0c46480646bdd
SuJin <kimsujin001127@gmail.com>
2024-02-26 15:28:32 +0900

    refactor: util로 변경

---
+++ src/utils/EngToKor.ts
@@ -0,0 +1,8 @@
+export const tag = [
+  { kor: '전체', eng: 'ALL' },
+  { kor: '인기글', eng: 'POPULAR' },
+  { kor: '잡담', eng: 'CHAT' },
+  { kor: '질문', eng: 'QUESTION' },
+  { kor: '정보', eng: 'INFORMATION' },
+];
+
