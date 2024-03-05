8fba6bce0fb275cfc6311643cb19972d22a1e118
JooHui-void <elis100@naver.com>
2024-03-06 04:21:12 +0900

    feat: 네비게이터 추가

--- src/pages/funsystem/index.page.tsx
+++ src/pages/funsystem/index.page.tsx
@@ -1,9 +1,10 @@
 import TopTab from '@/components/notice/TopTab';
+import MainLayout from '../layout';
 
 export default function Home() {
   return (
-    <>
+    <MainLayout>
       <TopTab type="펀시스템" />
-    </>
+    </MainLayout>
   );
 }
--- src/pages/notice/index.page.tsx
+++ src/pages/notice/index.page.tsx
@@ -1,9 +1,11 @@
+import Main from '@/components/main';
 import TopTab from '@/components/notice/TopTab';
+import MainLayout from '../layout';
 
 export default function Home() {
   return (
-    <>
+    <MainLayout>
       <TopTab type="공지사항" />
-    </>
+    </MainLayout>
   );
 }
