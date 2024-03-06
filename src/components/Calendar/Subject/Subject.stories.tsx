97efbd2dc892fd9955007a64d2f05c86f1050e32
halfmoon-mind <5338095@gmail.com>
2024-03-07 01:04:01 +0900

    fix: build 안되는 문제해결

--- src/components/Calendar/Subject/Subject.stories.tsx
+++ src/components/Calendar/Subject/Subject.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Subject from './Subject';
+import { CalendarType } from '../CalendarScreen/CalendarScreen';
 
 const meta: Meta<typeof Subject> = {
   title: 'Calendar/Subject',
@@ -13,23 +14,21 @@ type Story = StoryObj<typeof Subject>;
 export const SubjectExample: Story = {
   args: {
     subject: {
-      id: 1,
-      title: '웹프로그래밍',
-      color: '#FF7171',
-      tasks: [
+      course: '웹프로그래밍',
+      calendarResponses: [
         {
           id: 1,
-          title: 'test',
-          dueDate: '2022-10-10T09:00:00.000Z',
-          isFinished: false,
-          type: 'test',
+          name: 'test',
+          dueAt: '2022-10-10T09:00:00.000Z',
+          isCompleted: false,
+          type: CalendarType.QUIZ,
         },
         {
           id: 1,
-          title: 'test',
-          dueDate: '2022-10-10T09:00:00.000Z',
-          isFinished: false,
-          type: 'test',
+          name: 'test',
+          dueAt: '2022-10-10T09:00:00.000Z',
+          isCompleted: false,
+          type: CalendarType.QUIZ,
         },
       ],
     },
--- src/components/Calendar/SubjectTitle/SubjectTitle.stories.tsx
+++ src/components/Calendar/SubjectTitle/SubjectTitle.stories.tsx
@@ -11,12 +11,5 @@ export default meta;
 type Story = StoryObj<typeof SubjectTitle>;
 
 export const SubjectTitleExamples: Story = {
-  args: {
-    subject: {
-      id: 1,
-      title: '웹프로그래밍',
-      color: '#FF7171',
-      tasks: [],
-    },
-  },
+  args: { title: '웹프로그래밍' },
 };
