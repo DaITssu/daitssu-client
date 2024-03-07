31d470bcd8f2fde8f7fc7e47a065fb0235d2c4ae
JooHui-void <elis100@naver.com>
2024-03-07 23:00:22 +0900

    fix: 포멧팅 제거

--- src/components/community/list/CommunityListItem.tsx
+++ src/components/community/list/CommunityListItem.tsx
@@ -1,13 +1,14 @@
-import * as styles from './CommunityListItem.style';
-import React from 'react';
-import Link from 'next/link';
-import bubble from '@icons/icon/Icon18/SmallComment.svg';
 
-const changeDate = (timestampString: string) => {
+import * as styles from "./CommunityListItem.style";
+import React from 'react';
+import Link from "next/link";
+import bubble from "@icons/icon/Icon18/SmallComment.svg"
+
+const changeDate =(timestampString : string) =>{
   const nowDate = new Date();
   const timestamp = new Date(timestampString);
 
-  const timeDiff: number = nowDate.getTime() - timestamp.getTime();
+  const timeDiff:number = nowDate.getTime() - timestamp.getTime();
   const formatter = new Intl.RelativeTimeFormat('ko', { numeric: 'auto' });
 
   if (timeDiff < 3600000) {
@@ -18,47 +19,55 @@ const changeDate = (timestampString: string) => {
     const datePart = timestamp.toISOString().split('T')[0];
     return datePart;
   }
-};
+}
 
 export interface CommunityItemProps {
-  id: number;
+  id : number;
   topic: string;
   title: string;
-  content: string;
-  writerNickName: string;
-  updatedAt: string;
-  imageUrls: { url: string[] };
+  content : string;
+  writerNickName : string;
+  updatedAt : string;
+  imageUrls : {url:string[]};
   comments: number;
-  review: number;
+  review : number;
 }
 
-interface Props {
-  item: CommunityItemProps;
+interface Props{
+  item:CommunityItemProps;
 }
 
-const CommunityListItem: React.FC<Props> = ({ item }: Props) => {
-  return (
+const CommunityListItem:React.FC<Props> = ({item}:Props)=>{
+  
+  return(
     <>
-      <Link href={`/community/${item.id}`}>
+      <Link href = {`/community/${item.id}`}>
         <styles.ListBox>
-          <styles.Status>{item.topic}</styles.Status>
-          <styles.Title>{item.title}</styles.Title>
-          <styles.Contents>{item.content}</styles.Contents>
+          <styles.Status>
+            {item.topic}
+          </styles.Status>
+          <styles.Title>
+            {item.title}
+          </styles.Title>
+          <styles.Contents>
+            {item.content}
+          </styles.Contents>
           <styles.Bubble>
-            <styles.BubbleGroup
-              src={bubble}
-              width={18}
-              height={18}
-              alt={'chat_icon'}
-            />
-            <styles.Review>{item.comments}</styles.Review>
+            <styles.BubbleGroup src = {bubble} width={18} height={18} alt={'chat_icon'}/>
+            <styles.Review>
+              {item.comments}
+            </styles.Review>
           </styles.Bubble>
-
-          <styles.Minutes>{changeDate(item.updatedAt)}</styles.Minutes>
+          
+          <styles.Minutes>
+            {changeDate(item.updatedAt)}
+          </styles.Minutes>
         </styles.ListBox>
       </Link>
     </>
+    
   );
 };
+
 
 export default CommunityListItem;
