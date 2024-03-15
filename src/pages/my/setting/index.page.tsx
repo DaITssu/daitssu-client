c3bb9962bc85913961f6e93bb9aeb670b5910a68
halfmoon-mind <5338095@gmail.com>
2024-03-16 01:02:52 +0900

    fix: 내 정보에서 스타일 적용 안된 부분들 업데이트

--- src/pages/my/setting/index.page.tsx
+++ src/pages/my/setting/index.page.tsx
@@ -1,10 +1,11 @@
+import UtilityHeader from '@/components/common/Header/UtilityHeader';
 import Setting from '@/components/myPage/setting/setting';
 
 export default function Home() {
   return (
     <>
+      <UtilityHeader child="설정" />
       <Setting />
-      {/* <NavigationBar focusType={'MY_PAGE'} /> */}
     </>
   );
 }
