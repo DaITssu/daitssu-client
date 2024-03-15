12a62e159a3b51410f6b9d411423e6958a53d03d
halfmoon-mind <5338095@gmail.com>
2024-03-16 01:01:23 +0900

    refactor: bottom nav 네이밍 수정

--- src/components/common/Navbar/Navigation.ts
+++ src/components/common/Navbar/Navigation.ts
@@ -1,14 +1,14 @@
 export const NAV_LIST = {
   HOME: 'HOME',
   COMMUNITY: 'COMMUNITY',
-  ANNOUNCEMENT: 'ANNOUNCEMENT',
+  NOTICE: 'NOTICE',
   CALENDAR: 'CALENDAR',
   MY_PAGE: 'MY_PAGE',
 } as const;
 
 export const NAV_URL_LIST = {
   [NAV_LIST.HOME]: '/',
-  [NAV_LIST.ANNOUNCEMENT]: '/notice',
+  [NAV_LIST.NOTICE]: '/notice',
   [NAV_LIST.COMMUNITY]: '/community',
   [NAV_LIST.CALENDAR]: '/calendar',
   [NAV_LIST.MY_PAGE]: '/my',
